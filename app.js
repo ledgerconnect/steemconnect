60ed109f4dbb964243ce7726896c98ecc1918e32
adcpm <fabien@adcpm.com>
2016-08-07 13:02:29 +0700

    - Commit

--- app.js
+++ app.js
@@ -49,17 +49,14 @@ app.use(session({ secret: 'YOUR_SECRET_HERE', resave: false,  saveUninitialized:
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
-// Redirect to https://busy6.com
-/*
 app.use(function(req, res, next){
   var host = req.get('host');
-  if (host == 'localhost:3000' || (host == 'busy6.com' && req.secure)) {
+  if (host == 'localhost:3000' || (host == 'steemconnect.com' && req.secure)) {
     next();
   } else {
-    res.redirect('https://busy6.com');
+    res.redirect('https://steemconnect.com');
   }
 });
-*/
 
 // Enable CORS
 app.use(cors());
