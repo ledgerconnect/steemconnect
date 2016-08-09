c6f63a81840df785d558184d816ac7f4254f7cdb
adcpm <fabien@adcpm.com>
2016-08-10 04:24:09 +0700

    - Commit

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-var utils = require('./../lib/utils'),
+var Steem = require('steem'),
+  utils = require('./../lib/utils'),
+  auth = require('./../lib/auth'),
   bcrypt = require('bcrypt'),
-  jwt = require('jsonwebtoken'),
-  steemConnect = require('steemconnect');
+  jwt = require('jsonwebtoken');
 
 router.all('/login', function(req, res) {
   var username = req.body.username || req.query.username;
   var password = req.query.password || req.query.password;
-  var isValid = steemConnect.isValid(username, password, {posting: [['STM7SWWzw9LbzMvvS6WukeXNSqNSZAG5GJbh5u88DT7jGCWdrvgh1', 1]]});
-  if (isValid) {
-    var token = utils.generateToken({name: username});
-    res.json({
-      user: username,
-      token: token
-    });
-  } else {
-    return res.status(404).json({
-      error: true,
-      errorCode: 404,
-      errorMessage: 'Incorrect Username or Password'
-    });
-  }
+  var steem = new Steem();
+  steem.getAccounts([username], function(err, result){
+    var auths = {
+      owner: result[0].owner.key_auths,
+      active: result[0].active.key_auths,
+      posting: result[0].posting.key_auths
+    };
+    var isValid = auth.verify(username, password, auths);
+    if (isValid) {
+      var token = utils.generateToken({name: username});
+      res.json({
+        user: username,
+        token: token
+      });
+    } else {
+      return res.status(404).json({
+        error: true,
+        errorCode: 404,
+        errorMessage: 'Incorrect Username or Password'
+      });
+    }
+  });
 });
 
 router.all('/token', function(req, res) {
