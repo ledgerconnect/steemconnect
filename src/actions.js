dba1e1b3bc483085c759b0f5cb842d72c08d31a9
adcpm <fabien@adcpm.com>
2016-08-10 04:43:17 +0700

    - Commit

--- src/actions.js
+++ src/actions.js
@@ -9,32 +9,28 @@ module.exports = {
 			var req = {type: C.LOGIN_REQUEST};
 			Object.assign(req);
 			dispatch(req);
-			axios.get('//api.steemjs.com/getAccounts?names[]=' + username + '&ws=' + C.WS)
-				.then(response => {
-					if (_.has(response, 'data[0].owner')) {
-						var isValid = steemConnect.isValid(username, password, {
-							owner: response.data[0].owner.key_auths,
-							active: response.data[0].active.key_auths,
-							posting: response.data[0].posting.key_auths
-						});
-						var type = (isValid)? C.LOGIN_SUCCESS : C.LOGIN_FAILURE;
-						var errorMessage = (isValid)? '' : 'Incorrect Password';
-						var res = {
-							type: type,
-							user: response.data[0],
-							errorMessage: errorMessage
-						};
-					} else {
-						var res = {
-							type: C.LOGIN_FAILURE,
-							user: response.data[0],
-							errorMessage: 'Username does not exist'
-						};
-					}
-					console.log(res);
-					Object.assign(res);
-					dispatch(res);
-				});
+			steemConnect.token.get(username, password, function(err, token){
+				var res = {};
+				if (err) {
+					res = {
+						type: C.LOGIN_FAILURE,
+						user: {},
+						errorMessage: 'Incorrect Username or Password'
+					};
+				} else {
+					res = {
+						type: C.LOGIN_SUCCESS,
+						user: {
+							name: username,
+							token: token
+						},
+						errorMessage: ''
+					};
+				}
+				console.log(res);
+				Object.assign(res);
+				dispatch(res);
+			});
 		};
 	}
 };
