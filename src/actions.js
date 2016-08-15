716ba43d0e5b4229baeff285f72f87eaf5a8c333
adcpm <fabien@adcpm.com>
2016-08-15 17:40:54 +0700

    - Commit

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,6 @@
 var _ = require('lodash'),
 	axios = require('axios'),
 	steemConnect = require('steemconnect'),
-	jwt = require('jsonwebtoken'),
 	C = require('./constants');
 
 module.exports = {
@@ -10,8 +9,7 @@ module.exports = {
 			var req = {type: C.LOGIN_REQUEST};
 			Object.assign(req);
 			dispatch(req);
-			var self = this;
-			axios.get('//api.steemjs.com/getAccounts?names[]=' + username + '&ws=' + C.WS)
+			axios.get('//api.steemjs.com/getAccounts?names[]=' + username)
 				.then(response => {
 					if (_.has(response, 'data[0].owner')) {
 						if (steemConnect.auth.verify(username, password, {
@@ -19,16 +17,13 @@ module.exports = {
 							active: response.data[0].active.key_auths,
 							posting: response.data[0].posting.key_auths,
 						})) {
-							var code = self.generateCode(username, password);
-							steemConnect.token.get(code, function(err, token) {
-								res = {
-									type: C.LOGIN_SUCCESS,
-									user: {name: username, token: token},
-									errorMessage: ''
-								};
-								Object.assign(res);
-								dispatch(res);
-							});
+							res = {
+								type: C.LOGIN_SUCCESS,
+								user: {name: username},
+								errorMessage: ''
+							};
+							Object.assign(res);
+							dispatch(res);
 						} else {
 							res = {
 								type: C.LOGIN_FAILURE,
@@ -49,12 +44,5 @@ module.exports = {
 					}
 				});
 		}.bind(this);
-	},
-	generateCode: function(username, password) {
-		var auth = {
-			username: username,
-			password: password
-		};
-		return jwt.sign(auth, navigator.userAgent, {expiresIn: 5})
 	}
 };
