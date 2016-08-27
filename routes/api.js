feaeae1b05278cb1228487acd846144292f4080b
adcpm <fabien@adcpm.com>
2016-08-28 00:01:01 +0700

    - Commit

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,19 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+	router = express.Router(),
+	steem = require('steem');
+
+router.get('/api/verify', function(req, res, next) {
+	res.json({
+		username: 'siol',
+		permissions: ['verify', 'vote']
+	});
+});
+
+
+router.get('/api/vote', function(req, res, next) {
+	res.json({
+		error: ''
+	});
+});
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/*', function(req, res, next) {
-  var ip = req.ip || null;
-  var ua = req.headers['user-agent'] || null;
-  var language = req.headers['accept-language'] || null;
-  console.log(ua, language, ip);
   res.render('user/dashboard', {layout: 'user', title: 'Steem Connect'});
 });
 
