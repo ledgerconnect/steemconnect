d63d291c82e857a9ca2743b6aa437a19870600e3
adcpm <fabien@adcpm.com>
2016-08-29 13:34:16 +0700

    - Commit

--- routes/api.js
+++ routes/api.js
@@ -21,13 +21,15 @@ router.get('/api/verify', function(req, res, next) {
 
 router.get('/api/vote', function(req, res, next) {
 	res.json({
-		error: ''
+		error: '',
+		x: '1'
 	});
 });
 
 router.get('/api/comment', function(req, res, next) {
 	res.json({
-		error: ''
+		error: '',
+		x: '1'
 	});
 });
 
