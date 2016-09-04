87d1879aa8ce088f15a72c661cf1358e2552a81a
adcpm <fabien@adcpm.com>
2016-09-05 06:54:25 +0700

    Update profile placeholders

--- src/components/profile.js
+++ src/components/profile.js
@@ -73,41 +73,41 @@ var Dashboard = React.createClass({
 				<Header />
 				<div className="view-app">
 					<div className="block">
-						<form>
+						<form style={{maxWidth: '340px', margin: '0 auto'}}>
 							{avatarPlaceholder}
 							<fieldset className={"form-group"}>
-								<input autoFocus type="text" defaultValue={profile.name} placeholder="ned stark" className="form-control form-control-lg" ref="name" />
+								<input autoFocus type="text" defaultValue={profile.name} placeholder="Name" className="form-control form-control-lg" ref="name" />
 							</fieldset>
 							<fieldset className={"form-group " + (this.state.error.email ? 'has-danger' : '') }>
-								<input type="email" defaultValue={profile.email} placeholder="ned@thrones.io" className="form-control form-control-lg" ref="email" onBlur={this.validate.bind(this, 'email') }/>
+								<input type="email" defaultValue={profile.email} placeholder="Email" className="form-control form-control-lg" ref="email" onBlur={this.validate.bind(this, 'email') }/>
 								<div className="form-control-feedback">{this.state.error.email}</div>
 							</fieldset>
 							<fieldset className="form-group">
 								<label className="custom-control custom-radio">
 									<input name="radio" type="radio" value="male" className="custom-control-input" ref="gender_male" defaultChecked={profile.gender === 'male'}/>
 									<span className="custom-control-indicator"></span>
-									<span className="custom-control-description">male</span>
+									<span className="custom-control-description">Male</span>
 								</label>
 								<label className="custom-control custom-radio">
 									<input name="radio" type="radio" value="female" className="custom-control-input" ref="gender_female" defaultChecked={profile.gender === 'female'}/>
 									<span className="custom-control-indicator"></span>
-									<span className="custom-control-description">female</span>
+									<span className="custom-control-description">Female</span>
 								</label>
 							</fieldset>
 							<fieldset className={"form-group"}>
-								<textarea className="form-control form-control-lg" defaultValue={profile.about} placeholder="King in the north" rows="3" ref="about"></textarea>
+								<textarea className="form-control form-control-lg" defaultValue={profile.about} placeholder="About" rows="3" ref="about"></textarea>
 							</fieldset>
 							<fieldset className={"form-group"}>
-								<input type="text" placeholder="Ned" defaultValue={profile.first_name} className="form-control form-control-lg" ref="first_name" />
+								<input type="text" placeholder="First Name" defaultValue={profile.first_name} className="form-control form-control-lg" ref="first_name" />
 							</fieldset>
 							<fieldset className={"form-group"}>
-								<input type="text" placeholder="Stark" defaultValue={profile.last_name} className="form-control form-control-lg" ref="last_name" />
+								<input type="text" placeholder="Last Name" defaultValue={profile.last_name} className="form-control form-control-lg" ref="last_name" />
 							</fieldset>
 							<fieldset className={"form-group"}>
-								<input type="text" placeholder="Winterfell" defaultValue={profile.location} className="form-control form-control-lg" ref="location" />
+								<input type="text" placeholder="Location" defaultValue={profile.location} className="form-control form-control-lg" ref="location" />
 							</fieldset>
 							<fieldset className={"form-group " + (this.state.error.website ? 'has-danger' : '') }>
-								<input type="text"  defaultValue={profile.website} placeholder="website: https://steemconnect.com" className="form-control form-control-lg" ref="website" onBlur={this.validate.bind(this, 'website') } />
+								<input type="text"  defaultValue={profile.website} placeholder="Website" className="form-control form-control-lg" ref="website" onBlur={this.validate.bind(this, 'website') } />
 								<div className="form-control-feedback">{this.state.error.website}</div>
 							</fieldset>
 							<fieldset className="form-group"><button className="btn btn-primary" onClick={this.save}>Save</button></fieldset>
