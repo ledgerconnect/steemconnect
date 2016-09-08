02f74b1281074d59ffb797d4704f3aa9f5b70e45
adcpm <fabien@adcpm.com>
2016-09-09 06:45:57 +0700

    Add cover background

--- src/containers/edit-image-header.js
+++ src/containers/edit-image-header.js
@@ -2,21 +2,17 @@ const React = require('react'),
     Dropzone = require('react-dropzone');
 
 module.exports = (props) => {
-    if (props.onDrop) {
-        //Editable
-        return <div className="cover">
+    return (props.onDrop)?
+        <div className="cover" style={{backgroundImage: 'url(' + props.cover + ')'}}>
             <Dropzone className="avatar" onDrop={(files) => props.onDrop(files, 'profile_image') } accept='image/*'>
-                <a className="placeholder"><i className="icon icon-md material-icons">file_upload</i> Edit</a>
-                <img src={props.avatar}/>
+            <a className="placeholder"><i className="icon icon-md material-icons">file_upload</i> Edit</a>
+            <img src={props.avatar}/>
             </Dropzone>
             <Dropzone className='x' onDrop={(files) => props.onDrop(files, 'cover_image') } accept='image/*'><i className="icon icon-md material-icons">file_upload</i> Edit Cover</Dropzone>
-        </div>
-    } else {
-        //ReadOnly
-        return <div className="cover">
+        </div> :
+        <div className="cover" style={{backgroundImage: 'url(' + props.cover + ')'}}>
             <div className="avatar">
                 <img src={props.avatar}/>
             </div>
         </div>
-    }
-}
+};
