50a428a8bfb47304151941beae08e61005b8c882
adcpm <fabien@bonustrack.co>
2016-09-11 01:40:58 +0700

    Fix mistake param

--- src/containers/cover.js
+++ src/containers/cover.js
@@ -3,17 +3,17 @@ const React = require('react'),
     Dropzone = require('react-dropzone');
 
 module.exports = (props) => {
-    return <div className="cover pam" style={{ backgroundImage: 'url(' + `https://img.busy6.com/@${props.cover}/cover` + ')' }}>
+    return <div className="cover pam" style={{ backgroundImage: 'url(' + `https://img.busy6.com/@${props.username}/cover` + ')' }}>
         {props.onDrop? <div>
             <Dropzone className="avatar" onDrop={(files) => props.onDrop(files, 'profile_image') } accept='image/*'>
                 <a className="placeholder"><i className="icon icon-md material-icons">file_upload</i> Edit</a>
-                <img src={`https://img.busy6.com/@${props.name}`}/>
+                <img src={`https://img.busy6.com/@${props.username}`}/>
             </Dropzone>
             <Dropzone className="cover-edit" onDrop={(files) => props.onDrop(files, 'cover_image') } accept='image/*'>
                 <i className="icon icon-md material-icons">file_upload</i> Edit Cover
             </Dropzone>
         </div> : <div className="avatar">
-            <img src={`https://img.busy6.com/@${props.name}`}/>
+            <img src={`https://img.busy6.com/@${props.username}`}/>
         </div>}
         {!_.isUndefined(props.username) && <h3>@{props.username}</h3>}
         </div>
