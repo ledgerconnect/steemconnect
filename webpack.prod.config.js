14a88e2a50fd4a1d3ce724013252007959c21fe1
p0o <pooria_az@ymail.com>
2016-09-16 01:22:03 +0800

    re-configure webpack configs for sass support

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,50 +2,58 @@
 
 var path = require('path');
 var webpack = require('webpack');
-// var Visualizer = require('webpack-visualizer-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    devtool: 'cheap-module-source-map',
-    entry: [
-        path.join(__dirname, 'src/index.js')
+  devtool: 'cheap-module-source-map',
+  entry: [
+    path.join(__dirname, 'src/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/public/js'),
+    filename: 'app.min.js',
+    publicPath: '/js'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        // This has effect on the react lib size
+        'NODE_ENV': JSON.stringify('production'),
+      }
+    }),
+    new ExtractTextPlugin('../css/base.css'),
+    // new Visualizer()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          "presets": ["react", "es2015", "stage-0"]
+        }
+      },
+      {
+        test: /\.json?$/,
+        loader: 'json'
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css!autoprefixer-loader?browsers=last 2 version!sass'
+        )
+      }
     ],
-    output: {
-        path: path.join(__dirname, '/public/js'),
-        filename: 'app.min.js',
-        publicPath: '/js'
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.optimize.AggressiveMergingPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                // This has effect on the react lib size
-                'NODE_ENV': JSON.stringify('production'),
-            }
-        }),
-        // new Visualizer()
-    ],
-    module: {
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                "presets": ["react", "es2015", "stage-0"]
-            }
-        }, {
-                test: /\.json?$/,
-                loader: 'json'
-            }, {
-                test: /\.css$/,
-                loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
-            }]
-    }
+  },
 };
