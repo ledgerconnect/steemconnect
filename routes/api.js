d7caadac4592f005f505141e46f3438976375971
Nilesh Suthar <nileshsuthar@live.in>
2016-09-16 16:34:31 +0530

    Update Api Methods to use token

--- routes/api.js
+++ routes/api.js
@@ -62,48 +62,32 @@ router.get('/api/verify', function (req, res, next) {
 });
 
 router.get('/api/vote', function (req, res, next) {
-	var auth = (req.cookies['_sc_a']) ? JSON.parse(req.cookies['_sc_a']) : cookie.get();
-	if (_.has(auth, 'username')) {
-		var voter = req.query.voter,
-			author = req.query.author,
-			permlink = req.query.permlink,
-			weight = parseInt(req.query.weight);
-		steem.broadcast.vote(auth.wif, voter, author, permlink, weight, function (err, result) {
-			res.json({
-				error: err,
-				result: result
-			});
-		});
-
-	} else {
+	var voter = req.query.voter,
+		author = req.query.author,
+		permlink = req.query.permlink,
+		weight = parseInt(req.query.weight);
+	steem.broadcast.vote(req.wif, voter, author, permlink, weight, function (err, result) {
 		res.json({
-			isAuthenticated: false
-		})
-	}
+			error: err,
+			result: result
+		});
+	});
 });
 
 router.get('/api/comment', function (req, res, next) {
-	var auth = (req.cookies['_sc_a']) ? JSON.parse(req.cookies['_sc_a']) : cookie.get();
-	if (_.has(auth, 'username')) {
-		var parentAuthor = req.query.parentAuthor,
-			parentPermlink = req.query.parentPermlink,
-			author = req.query.author,
-			permlink = req.query.permlink,
-			title = req.query.title,
-			body = req.query.body,
-			jsonMetadata = req.query.jsonMetadata;
-		steem.broadcast.comment(auth.wif, parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata, function (err, result) {
-			res.json({
-				error: err,
-				result: result
-			});
-		});
-
-	} else {
+	var parentAuthor = req.query.parentAuthor,
+		parentPermlink = req.query.parentPermlink,
+		author = req.query.author,
+		permlink = req.query.permlink,
+		title = req.query.title,
+		body = req.query.body,
+		jsonMetadata = req.query.jsonMetadata;
+	steem.broadcast.comment(req.wif, parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata, function (err, result) {
 		res.json({
-			isAuthenticated: false
-		})
-	}
+			error: err,
+			result: result
+		});
+	});
 });
 
 router.get('/api/accountCreate', function (req, res, next) {
@@ -115,7 +99,7 @@ router.get('/api/accountCreate', function (req, res, next) {
 		posting = req.query.posting,
 		memoKey = req.query.memoKey,
 		jsonMetadata = req.query.jsonMetadata;
-	steem.broadcast.accountCreate(auth.wif, fee, creator, newAccountName, owner, active, posting, memoKey, jsonMetadata, function (err, result) {
+	steem.broadcast.accountCreate(req.wif, fee, creator, newAccountName, owner, active, posting, memoKey, jsonMetadata, function (err, result) {
 		res.json({
 			error: err,
 			result: result
@@ -124,24 +108,16 @@ router.get('/api/accountCreate', function (req, res, next) {
 });
 
 router.get('/api/customJson', function (req, res, next) {
-	var auth = (req.cookies['_sc_a']) ? JSON.parse(req.cookies['_sc_a']) : cookie.get();
-	if (_.has(auth, 'username')) {
-		var requiredAuths = req.query.requiredAuths,
-			requiredPostingAuths = req.query.requiredPostingAuths,
-			id = req.query.id,
-			json = req.query.json;
-		steem.broadcast.customJson(auth.wif, requiredAuths, requiredPostingAuths, id, json, function (err, result) {
-			res.json({
-				error: err,
-				result: result
-			});
-		});
-
-	} else {
+	var requiredAuths = req.query.requiredAuths,
+		requiredPostingAuths = req.query.requiredPostingAuths,
+		id = req.query.id,
+		json = req.query.json;
+	steem.broadcast.customJson(req.wif, requiredAuths, requiredPostingAuths, id, json, function (err, result) {
 		res.json({
-			isAuthenticated: false
-		})
-	}
+			error: err,
+			result: result
+		});
+	});
 });
 
 module.exports = router;
