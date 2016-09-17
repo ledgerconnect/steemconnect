ca7652079bf084a8d819e2cb5664ca224d5e9212
Nilesh Suthar <nileshsuthar@live.in>
2016-09-17 10:49:01 +0530

    Fix linting for middleware

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,40 +1,40 @@
-const _ = require('lodash'),
-    {getSecretKeyForClientId, decrypMessage} = require('../lib/utils'),
-    jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const { getSecretKeyForClientId, decrypMessage } = require('../lib/utils');
+const jwt = require('jsonwebtoken');
 
 function verifyAuth(req, res, next) {
-    if (req.cookies.token && typeof req.headers.authorization === 'undefined') {
-        req.headers.authorization = 'Bearer ' + req.cookies.token;
-    }
-    if (typeof req.headers.authorization !== 'undefined' && (req.headers.authorization.search('Bearer ') === 0)) {
-        let token = req.headers.authorization.substring('Bearer '.length)
-        jwt.verify(token, process.env.JWT_SECRET, (err, jwtData) => {
-            if (err) {
-                console.log('err', err);
-                res.sendStatus(401);
-            } else {
-                let computeSecret = getSecretKeyForClientId(process.env.PUBLIC_KEY);
-                let message = decrypMessage(jwtData.secret, computeSecret);
-                message = JSON.parse(message);
-                if (message.username == jwtData.username) {
-                    _.each(jwtData, (value, key) => {
-                        req[key] = value;
-                    })
-                    _.each(message, (value, key) => {
-                        req[key] = value;
-                    })
-                    next();
-                } else {
-                    res.sendStatus(401);
-                }
-
-            }
-        })
-    } else {
+  /* TODO Add Origin Check */
+  if (req.cookies.token && typeof req.headers.authorization === 'undefined') {
+    req.headers.authorization = `Bearer ${req.cookies.token}`; // eslint-disable-line no-param-reassign
+  }
+  if (typeof req.headers.authorization !== 'undefined' && (req.headers.authorization.search('Bearer ') === 0)) {
+    const token = req.headers.authorization.substring('Bearer '.length);
+    jwt.verify(token, process.env.JWT_SECRET, (err, jwtData) => {
+      if (err) {
+        // console.log('err', err);
         res.sendStatus(401);
-    }
+      } else {
+        const computeSecret = getSecretKeyForClientId(process.env.PUBLIC_KEY);
+        let message = decrypMessage(jwtData.secret, computeSecret);
+        message = JSON.parse(message);
+        if (message.username === jwtData.username) {
+          _.each(jwtData, (value, key) => {
+            req[key] = value; // eslint-disable-line no-param-reassign
+          });
+          _.each(message, (value, key) => {
+            req[key] = value; // eslint-disable-line no-param-reassign
+          });
+          next();
+        } else {
+          res.sendStatus(401);
+        }
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
 }
 
 module.exports = {
-    verifyAuth
-}
+  verifyAuth,
+};
