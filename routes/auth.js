e46b792912d3b44cb8284c06ca0525f6dc8738d5
Nilesh Suthar <nileshsuthar@live.in>
2016-09-22 00:21:10 +0530

    Respect errorcode in auth Api

--- routes/auth.js
+++ routes/auth.js
@@ -16,15 +16,15 @@ router.get('/auth/login', (req, res) => {
   const computeSecret = getSecretKeyForClientId(process.env.PUBLIC_KEY);
   steem.api.getAccounts([username], (err, result) => {
     if (err) {
-      res.send({ error: JSON.stringify(err) });
+      res.status(500).send({ error: JSON.stringify(err) });
     } else if (result.length === 0) {
-      res.send({ error: 'Incorrect Username' });
+      res.status(500).send({ error: 'Incorrect Username' });
     } else if (result[0] && steemAuth.wifIsValid(wif, result[0].posting.key_auths[0][0])) {
       const secret = encryptMessage(JSON.stringify({ username, wif }), computeSecret);
       const auth = jwt.sign({ username, secret }, process.env.JWT_SECRET, { expiresIn: '36h' });
       res.send({ userAccount: result[0], auth });
     } else {
-      res.send({ error: 'Incorrect Password' });
+      res.status(500).send({ error: 'Incorrect Password' });
     }
   });
 });
@@ -40,20 +40,24 @@ router.post('/auth/create', verifyAuth, (req, res) => {
     const isWif = steemAuth.isWif(appOwnerWif);
     const ownerKey = (isWif) ? appOwnerWif : steemAuth.toWif(appUserName, appOwnerWif, 'owner');
     if (err) {
-      res.json({ error: JSON.stringify(err) });
+      res.status(500).send({ error: JSON.stringify(err) });
     } else {
-      const user = result[0];
-      const jsonMetadata = getJSONMetadata(user);
-      const private_metadata = encryptMessage(JSON.stringify({ origins, redirect_urls }), clientSecret); // eslint-disable-line
-      jsonMetadata.app = { name: appName, author, permissions, private_metadata };
-      steem.broadcast.accountUpdate(ownerKey, appUserName, undefined, undefined, undefined,
-        user.memo_key, jsonMetadata, (accountUpdateErr) => {
-          if (accountUpdateErr) {
-            res.json({ error: JSON.stringify(accountUpdateErr) });
-          } else {
-            res.json({ clientId, clientSecret });
-          }
-        });
+      try {
+        const user = result[0];
+        const jsonMetadata = getJSONMetadata(user);
+        const private_metadata = encryptMessage(JSON.stringify({ origins, redirect_urls }), clientSecret); // eslint-disable-line
+        jsonMetadata.app = { name: appName, author, permissions, private_metadata };
+        steem.broadcast.accountUpdate(ownerKey, appUserName, undefined, undefined, undefined,
+          user.memo_key, jsonMetadata, (accountUpdateErr) => {
+            if (accountUpdateErr) {
+              res.status(500).send({ error: JSON.stringify(accountUpdateErr) });
+            } else {
+              res.json({ clientId, clientSecret });
+            }
+          });
+      } catch (e) {
+        res.status(500).send({ error: JSON.stringify(e) });
+      }
     }
   });
 });
@@ -62,7 +66,7 @@ router.get('/auth/authorize', verifyAuth, (req, res) => {
   const { appUserName, clientId, redirect_url, scope } = req.query;
   steem.api.getAccounts([appUserName], (err, result) => {
     if (err) {
-      res.json({ error: JSON.stringify(err) });
+      res.status(500).send({ error: JSON.stringify(err) });
     } else {
       try {
         const clientSecret = getSecretKeyForClientId(clientId);
@@ -91,7 +95,7 @@ router.get('/auth/authorize', verifyAuth, (req, res) => {
         } else if (e.message.search('json_metadata') >= 0) {
           message = 'Invalid appName';
         }
-        res.json({ error: message });
+        res.status(500).send({ error: message });
       }
     }
   });
