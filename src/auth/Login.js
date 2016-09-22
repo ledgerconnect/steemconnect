78f2164857f7138797544a6ac3cec0207ec5a727
Nilesh Suthar <nileshsuthar@live.in>
2016-09-22 10:06:29 +0530

    Password Autofocus

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -42,10 +42,10 @@ class Login extends Component {
           {selectedUser && <EditImageHeader username={selectedUser} />}
           <form className="form pvx mhl" onSubmit={this.handleSubmit}>
             <fieldset className="form-group">
-              <input autoFocus type={selectedUser ? 'hidden' : 'text'} placeholder="Username" defaultValue={selectedUser} className="form-control form-control-lg" ref={(c) => { this.username = c; } } />
+              <input type={selectedUser ? 'hidden' : 'text'} placeholder="Username" defaultValue={selectedUser} className="form-control form-control-lg" ref={(c) => { this.username = c; }} />
             </fieldset>
             <fieldset className="form-group">
-              <input type="password" placeholder="Password or posting WIF" className="form-control form-control-lg" ref={(c) => { this.passwordOrWif = c; } } />
+              <input autoFocus type="password" placeholder="Password or posting WIF" className="form-control form-control-lg" ref={(c) => { this.passwordOrWif = c; }} />
             </fieldset>
             {this.props.auth.errorMessage &&
               <ul className="errorMessages">
