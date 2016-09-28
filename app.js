e6eefe6042e8ace553ae32591d0c4bd436407133
adcpm <fabien@bonustrack.co>
2016-09-29 00:42:04 +0700

    Remove raw env variables

--- app.js
+++ app.js
@@ -20,13 +20,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('./webpack')(app); // eslint-disable-line
 }
 
-/* TODO For testing remove later */
 process.env.SESSION_SECRET = process.env.SESSION_SECRET || 'some!!@@secret123';
-process.env.PRIME = 'eca59a04b80707d8bf72739b9e97f0692ef5614c83128e520348aa3cd81c1e3b';
-process.env.PUBLIC_KEY = '0467e93f4a008220dc4f89a8446ffb96592b8a64222b71839392263b8eb9ccd1c8be169261eb41069b9a4e19e424b5d10749ce2dd8b0dafcc188a9a0865f768b83';
-process.env.PRIVATE_KEY = '70e97a7091f238948dd6efecbee658b1fb4b684fd6883cea4ff50cfc149b6d3a';
-process.env.JWT_SECRET = 'a2a30bd6fc5f44c1b30439db8b7ef0833effda2e505fbed65e96e889919f1813';
-process.env.CRYPTO_MOD = 'secp256k1';
 
 // view engine setup
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
