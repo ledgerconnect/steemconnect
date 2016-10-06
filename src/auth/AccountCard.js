d490b763a21372428be4363a6520538f97a8fbe5
adcpm <fabien@bonustrack.co>
2016-10-06 12:14:24 +0700

    Improve design

--- src/auth/AccountCard.js
+++ src/auth/AccountCard.js
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react';
 const AccountCard = (props) => {
   return (<div className="account-card" style={{ backgroundImage: `url(https://img.busy6.com/@${props.username}/cover` }}>
     <a><img className="profile-image" alt={`@${props.username}`} src={`https://img.busy6.com/@${props.username}`} /></a>
-    <h2>
+    <h2 className="mts">
       @{props.username} <a className="icon icon-sm material-icons"
         onClick={(event) => { event.preventDefault(); props.onDelete(props.username); return false; }}
       >remove_circle</a>
