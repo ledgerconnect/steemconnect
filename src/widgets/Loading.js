167ecc2dd140d38a4a5605e237a35b2403307086
adcpm <fabien@bonustrack.co>
2016-10-06 13:47:32 +0700

    Improve loading design

--- src/widgets/Loading.js
+++ src/widgets/Loading.js
@@ -1,17 +1,13 @@
-import React, { PropTypes } from 'react';
+const React = require('react');
 
-const Loading = (props) => {
-  let className = (props.color === 'white') ? 'loading-white' : 'loading';
-  className += ' align-center';
-  return (
-    <div className="pam pbl">
-      <div className={className}><span>.</span><span>.</span><span>.</span></div>
-    </div>
-  );
-};
-
-Loading.propTypes = {
-  color: PropTypes.string,
-};
-
-export default Loading;
+module.exports = React.createClass({
+  render() {
+    let className = (this.props.color === 'white') ? 'loading-white' : 'loading';
+    className += ' align-center';
+    return (
+      <div><div className={className}>
+        <span>●</span><span>●</span><span>●</span></div>
+      </div>
+    );
+  }
+});
