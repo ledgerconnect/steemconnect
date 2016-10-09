a48500d4b0b835fd67fa0c05cb945515e2759830
adcpm <fabien@bonustrack.co>
2016-10-10 00:27:28 +0700

    Bring onDelete logic into AccountCard Move "not you" to Login component

--- src/auth/AccountCard.js
+++ src/auth/AccountCard.js
@@ -1,28 +1,51 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import _ from 'lodash';
+import cookie from '../../lib/cookie';
 
-const AccountCard = (props) => {
-  return (<div className="account-card" style={{ backgroundImage: `url(https://img.busy6.com/@${props.username}/cover` }}>
-  <span className="change-user">
-    <a onClick={props.showUserList}>Not you?</a>
-  </span>
- <a><img className="profile-image" alt={`@${props.username}`} src={`https://img.busy6.com/@${props.username}`} /></a>
-    <h2 className="mts">
-      @{props.username} <a
+class AccountCard extends Component {
+  constructor(props) {
+    super(props);
+    let lastUserList = cookie.get('last_users');
+    if (!_.isArray(lastUserList)) {
+      lastUserList = [];
+    }
+    this.state = { lastUserList };
+  }
+
+  onDelete = (username) => {
+    const { lastUserList } = this.state;
+    const index = lastUserList.indexOf(username);
+    if (index >= 0) {
+      lastUserList.splice(index, 1);
+      this.updateUser(lastUserList);
+    }
+  }
+
+  updateUser = (lastUserList) => {
+    this.setState({ lastUserList });
+    cookie.save(lastUserList, 'last_users');
+  }
+
+  render() {
+    return (<div className="account-card" style={{ backgroundImage: `url(https://img.busy6.com/@${this.props.username}/cover` }}>
+      <a><img className="profile-image" alt={`@${this.props.username}`} src={`https://img.busy6.com/@${this.props.username}`} /></a>
+      <h2 className="mts">
+        @{this.props.username} <a
         title="Remove Account"
         className="icon icon-sm material-icons"
         onClick={(event) => {
           event.preventDefault();
-          props.onDelete(props.username);
+          this.props.onDelete(this.props.username);
           return false;
         }}
       >remove</a>
-    </h2>
-  </div>);
-};
+      </h2>
+    </div>);
+  }
+}
 
 AccountCard.propTypes = {
-  username: PropTypes.string.isRequired,
-  onDelete: PropTypes.func,
+  username: PropTypes.string.isRequired
 };
 
 export default AccountCard;
