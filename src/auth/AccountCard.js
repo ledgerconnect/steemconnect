79f9828b38c460bd781e035ce47bfac1e8ce096e
adcpm <fabien@bonustrack.co>
2016-10-10 01:46:59 +0700

    Move delete user logic to cookie lib

--- src/auth/AccountCard.js
+++ src/auth/AccountCard.js
@@ -1,31 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import _ from 'lodash';
 import cookie from '../../lib/cookie';
 
 class AccountCard extends Component {
-  constructor(props) {
-    super(props);
-    let lastUserList = cookie.get('last_users');
-    if (!_.isArray(lastUserList)) {
-      lastUserList = [];
-    }
-    this.state = { lastUserList };
-  }
-
-  onDelete = (username) => {
-    const { lastUserList } = this.state;
-    const index = lastUserList.indexOf(username);
-    if (index >= 0) {
-      lastUserList.splice(index, 1);
-      this.updateUser(lastUserList);
-    }
-  }
-
-  updateUser = (lastUserList) => {
-    this.setState({ lastUserList });
-    cookie.save(lastUserList, 'last_users');
-  }
-
   render() {
     return (<div className="account-card" style={{ backgroundImage: `url(https://img.busy6.com/@${this.props.username}/cover` }}>
       <a><img className="profile-image" alt={`@${this.props.username}`} src={`https://img.busy6.com/@${this.props.username}`} /></a>
@@ -35,7 +11,7 @@ class AccountCard extends Component {
         className="icon icon-sm material-icons"
         onClick={(event) => {
           event.preventDefault();
-          this.props.onDelete(this.props.username);
+          cookie.deleteUser(this.props.username);
           return false;
         }}
       >remove</a>
