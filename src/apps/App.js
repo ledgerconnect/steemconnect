4cdd79ed5b9dab18a0b11925f724f492d315d86c
adcpm <fabien@bonustrack.co>
2016-10-12 13:12:25 +0700

    Move AppDetails to Apps folder with App.js file name

--- src/apps/App.js
+++ src/apps/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
-import Header from './../app/header';
-import apps from './../helpers/apps.json';
+import Header from '../app/header';
+import apps from '../helpers/apps.json';
 
 class AppDetails extends Component {
   render() {
--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import Authorize from './auth/Authorize';
 import Developer from './developer/Developer';
 import Dashboard from './dashboard/Dashboard';
 import Apps from './apps/Apps';
-import AppDetails from './appdetails/AppDetails';
+import App from './apps/App';
 import Payments from './payments/Payments';
 import Activity from './activity/Activity';
 
@@ -17,7 +17,7 @@ module.exports = (
     <Route path="/activity" component={Activity} />
     <Route path="/payments" component={Payments} />
     <Route path="/apps" component={Apps} />
-    <Route path="/apps/:app" component={AppDetails}/>
+    <Route path="/apps/:app" component={App}/>
     <Route path="/profile" component={Profile} />
     <Route path="/developers" component={Developer} />
     <Route path="/authorize/@:app" component={Authorize} />
