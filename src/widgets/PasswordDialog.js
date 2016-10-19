0732c99d38ecd01ae4505ad58c97630ed6b57027
Daniel Chmiel <daniel.chmiel@outlook.com>
2016-10-19 13:32:26 +0200

    Password dialog wireframe implementation

--- src/widgets/PasswordDialog.js
+++ src/widgets/PasswordDialog.js
@@ -11,7 +11,7 @@ class PasswordDialog extends Component {
   }
   render() {
     const { isUpdating, error } = this.props;
-    let saveText = isUpdating === true ? 'Saving' : 'Save';
+    let saveText = isUpdating === true ? 'Confirming' : 'Confirm';
     let message;
     let showSuccess;
     if (isUpdating === false && error === false) {
@@ -22,14 +22,28 @@ class PasswordDialog extends Component {
       message = <div>Incorrect Password</div>;
     }
     return (
-      <div className="password-dialog">
-        <i className="icon icon-md material-icons password-close" onClick={this.props.onClose}>close</i>
-        <fieldset className={"form-group"}>
-          {!showSuccess && <label htmlFor="passwordInput" className="message">Enter your password to update.</label>}
-          {!showSuccess && <input id="passwordInput" autoFocus type="password" placeholder="Password or owner WIF" className="form-control form-control-lg" ref={c => (this.passwordOrWif = c)} />}
-          {message}
-        </fieldset>
-        <fieldset className="form-group"><button disabled={isUpdating} className="btn btn-primary" onClick={this.savePassword}>{saveText}</button></fieldset>
+      <div className="dialog dialog-password">
+        <div className="dialog-content man maxl-ns">
+          <i className="icon icon-md material-icons dialog-close" onClick={this.props.onClose}>close</i>
+          <i className="icon icon-md material-icons dialog-lock-icon">lock</i>
+          <div className="dialog-header pvs mts">
+            <img className="profile-image dialog-logo" alt={`@${this.props.username}`} src={`https://img.busy6.com/@${this.props.username}`} />
+            <p className="man">@{this.props.username}</p>
+          </div>
+            <div className="form form-login dialog-form mbs">
+              <div className="input-group input-group-lg"> 
+                <span className="input-group-addon">
+                  <i className="icon icon-md material-icons">lock_outline</i>
+                </span>          
+                {!showSuccess && <input id="passwordInput" autoFocus type="password" placeholder="Password or owner WIF" className="form-control" ref={c => (this.passwordOrWif = c)} />}
+                {message}
+              </div>
+              <fieldset className="form-group man"><button disabled={isUpdating} className="btn btn-success form-submit" onClick={this.savePassword}>{saveText}</button></fieldset>
+            </div>
+          <p className="phl">
+            Your password or owner WIF are never saved by Steem Connect. <a href="#">Learn more</a>
+          </p>
+        </div>
       </div>
     );
   }
