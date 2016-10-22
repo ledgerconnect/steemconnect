56a1d0abeed61cd79f5d0c290dbc8a2e395d1c9b
Nilesh Suthar <nileshsuthar@live.in>
2016-10-22 11:51:50 +0530

    Added Loader

--- src/auth/Authorize.js
+++ src/auth/Authorize.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import { getAppDetails } from './authAction';
 import { accountUpdate, clearUpdatingResult } from '../actions';
 import PasswordDialog from './../widgets/PasswordDialog';
+import Loading from './../widgets/Loading';
 
 class Authorize extends React.Component {
   constructor(props) {
@@ -61,22 +62,27 @@ class Authorize extends React.Component {
     const { apps = {}, user = {} } = this.props.auth;
     const appDetails = apps[appName] || {};
     const { permissions: permissionList = [], redirect_url, error } = appDetails;
+    const isLoading = _.isEmpty(appDetails);
+    let errorOrLoading;
+    if (isLoading) {
+      errorOrLoading = <Loading />;
+    } else if (error) { errorOrLoading = <div>{error}</div>; }
+
 
     return (
       <div className="block">
         <div className="mbl"><img alt="app-logo" src={`https://img.busy6.com/@${appName}`} width="70" /></div>
-        {(error) ?
-          <div>{error}</div> :
-          (<div>
-            <p>The app <b>{appName}</b> is requesting permission to do the following: </p>
-            <ul className="mbm">
-              {permissionList.map(({ name, api }) => <div key={api}>
-                <input type="checkbox" className="form-check-input" ref={c => (this.permissions[api] = c)} defaultChecked="true" value={api} />
-                {name}
-              </div>)}
-            </ul>
-            <a onClick={() => this.authorizeUser(redirect_url, appName)} className="btn btn-primary mbm">Continue as @{user.name}</a>
-          </div>)
+        {(errorOrLoading)}
+        {(!errorOrLoading) && (<div>
+          <p>The app <b>{appName}</b> is requesting permission to do the following: </p>
+          <ul className="mbm">
+            {permissionList.map(({ name, api }) => <div key={api}>
+              <input type="checkbox" className="form-check-input" ref={c => (this.permissions[api] = c)} defaultChecked="true" value={api} />
+              {name}
+            </div>)}
+          </ul>
+          <a onClick={() => this.authorizeUser(redirect_url, appName)} className="btn btn-primary mbm">Continue as @{user.name}</a>
+        </div>)
         }
         {this.state.showPasswordDialog && <PasswordDialog
           isUpdating={false}
