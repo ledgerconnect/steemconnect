789e2e4073d62ff0ea338993191c38296ddcabcc
adcpm <fabien@bonustrack.co>
2016-10-24 17:04:36 +0700

    Clear summary

--- src/activity/Activity.js
+++ src/activity/Activity.js
@@ -1,91 +1,33 @@
+import { bindActionCreators } from 'redux';
 import React, { Component, PropTypes } from 'react';
-import Header from './../app/header';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import Header from '../app/header';
+import ActivityList from '../activity/ActivityList';
+import { getAccountHistory } from '../actions';
+import Loading from '../widgets/Loading';
 
 class Activity extends Component {
+  componentDidMount() {
+    this.props.getAccountHistory(this.props.auth.user.name, -1, 10);
+  }
   render() {
+    const { accountHistory } = this.props.auth.user;
     return (
       <div>
         <Header />
-
-        <section className="align-center profile-header">
-          <div className="container">
-            <h1>
-              Activity
-            </h1>
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </span>
-          </div>
-        </section>
-        <ul className="secondary-nav mbl">
-          <li>
-            <a href="#">
-              <i className="icon icon-md material-icons">event</i>
-              <span className="hidden-xs"> All Activity</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="icon icon-md material-icons">chat_bubble_outline</i>
-              <span className="hidden-xs"> Blog</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="icon icon-md material-icons">compare_arrows</i>
-              <span className="hidden-xs"> Transfers</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="icon icon-md material-icons">save</i>
-              <span className="hidden-xs"> Account Update</span>
-            </a>
-          </li>
-        </ul>
-
+        <h1>Activity</h1>
         <div className="container">
-          <div className="block block-activity">
-            <ul className="list list-activity">
-              <li className="list-element pam">
-                <div className="list-container">
-                  <h3 className="list-title man">Upvote</h3>
-                  <span className="list-description">For "New Advanced Formatting Features" by @dan</span>
-                </div>
-                <span className="list-date">28 September</span>
-              </li>
-              <li className="list-element pam">
-                <div className="list-container">
-                  <h3 className="list-title man">Post</h3>
-                  <span className="list-description">Oslo Steemit hackaton with Ned Scott</span>
-                </div>
-                <span className="list-date">today</span>
-              </li>
-              <li className="list-element pam">
-                <div className="list-container">
-                  <h3 className="list-title man">Transfer of 120.000 STEEM</h3>
-                  <span className="list-description">To @dan</span>
-                </div>
-                <span className="list-date">today</span>
-              </li>
-              <li className="list-element pam">
-                <div className="list-container">
-                  <h3 className="list-title man">Upvote</h3>
-                  <span className="list-description">For "It's time for a Steem Bounty System!" by @ned</span>
-                </div>
-                <span className="list-date">today</span>
-              </li>
-              <li className="list-element pam">
-                <div className="list-container">
-                  <h3 className="list-title man">Transfer of 56.00 SBD</h3>
-                  <span className="list-description">To @jesf</span>
-                </div>
-                <span className="list-date">today</span>
-              </li>
-            </ul>
-            <div className="list-more pas">
-              <a href="#">See more</a>
-            </div>
+          <div className="block block-dashboard">
+            {accountHistory ? <div>
+              <ul className="list list-activity">
+                {accountHistory && _.sortBy(accountHistory, 'timestamp').reverse().map(([id, transaction]) =>
+                  <ActivityList key={id} id={id} transaction={transaction} />) }
+              </ul>
+              <div className="list-more pas">
+                <a href="#">See more</a>
+              </div>
+            </div> : <Loading />}
           </div>
         </div>
       </div>
@@ -93,4 +35,12 @@ class Activity extends Component {
   }
 }
 
-export default Activity;
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getAccountHistory: bindActionCreators(getAccountHistory, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Activity);
--- src/summary/Summary.js
+++ src/summary/Summary.js
@@ -1,51 +1,11 @@
-import { bindActionCreators } from 'redux';
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import _ from 'lodash';
-import Header from './../app/header';
-import Activity from '../activity/ActivityList';
-import { getAccountHistory } from './../actions';
+import React from 'react';
 
-
-class Dashboard extends Component {
-  componentDidMount() {
-    this.props.getAccountHistory(this.props.auth.user.name, -1, 10);
-  }
+module.exports = React.createClass({
   render() {
-    const { accountHistory } = this.props.auth.user;
     return (
       <div>
-        <Header />
-        <h2 className="ptl pbm">Last Activity</h2>
-        <div className="container">
-          <div className="block block-dashboard">
-            <ul className="list list-activity">
-              {accountHistory && _.sortBy(accountHistory, 'timestamp').reverse().map(([id, transaction]) =>
-                <Activity key={id} id={id} transaction={transaction} />) }
-            </ul>
-            <div className="list-more pas">
-              <a href="#">See more</a>
-            </div>
-          </div>
-        </div>
+        <h1>Welcome onboard</h1>
       </div>
     );
   }
-}
-
-Dashboard.propTypes = {
-  auth: PropTypes.shape({
-    user: PropTypes.object.isRequired,
-  }),
-  getAccountHistory: PropTypes.func,
-};
-
-const mapStateToProps = state => ({
-  auth: state.auth,
 });
-
-const mapDispatchToProps = dispatch => ({
-  getAccountHistory: bindActionCreators(getAccountHistory, dispatch),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
