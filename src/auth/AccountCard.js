062545e8054d9da2c4d017c690bdcb7a0f838070
adcpm <fabien@bonustrack.co>
2016-11-16 22:54:58 +0100

    Remove cover from card

--- src/auth/AccountCard.js
+++ src/auth/AccountCard.js
@@ -1,19 +1,15 @@
 import React, { Component, PropTypes } from 'react';
-import cookie from './../../lib/cookie';
+import cookie from '../../lib/cookie';
+import Avatar from '../widgets/Avatar';
 
 class AccountCard extends Component {
   render() {
-    var userBackground = {
-      background: `radial-gradient(circle at 50% 0%, rgba(0, 0, 0, 0.0980392), rgba(0, 0, 0, 0.6)), url("https://img.busy.org/@${this.props.username}/cover")`,
-      backgroundSize: `cover`,
-      backgroundPosition: `center`,
-    };
-    return (<div className="account-card" style={userBackground}>
-      <a><img className="profile-image" alt={`@${this.props.username}`} src={`https://img.busy.org/@${this.props.username}`} /></a>
+    return (<div className="account-card">
+      <Avatar xl username={this.props.username} />
       <h2 className="mts">
         @{this.props.username} <a
         title="Remove Account"
-        className="icon icon-sm material-icons"
+        className="hide icon icon-sm material-icons"
         onClick={(event) => {
           event.preventDefault();
           cookie.deleteUser(this.props.username);
