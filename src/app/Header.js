15a899e99167d9aec98fede2756ad4d0676c1721
adcpm <fabien@bonustrack.co>
2016-11-16 21:47:16 +0100

    Add open sidebar button

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,22 +1,16 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { hideSidebar, showSidebar } from './appAction';
 
 const Header = props => <header>
   <div className="top-nav">
+    {!props.app.sidebarIsVisible && <a className="left" onClick={() => props.showSidebar()}><i className="icon icon-md icon-menu material-icons">menu</i></a>}
     <div className="section-content">
       <Link to="/" onlyActiveOnIndex activeClassName="active"><img alt="logo" className="logo" src="/img/logo.svg" /></Link>
     </div>
   </div>
 </header>;
-
-Header.propTypes = {
-  app: PropTypes.shape({
-    sidebarIsVisible: PropTypes.bool,
-  }),
-  hideSidebar: PropTypes.func,
-};
 
 const mapStateToProps = state => ({
   app: state.app,
