9b8c58795677cc3fe0b10e4c0a33720edb485210
Nilesh Suthar <nileshsuthar@live.in>
2016-11-16 12:00:19 +0530

    Update design

--- src/payments/Payments.js
+++ src/payments/Payments.js
@@ -9,7 +9,7 @@ import PasswordDialog from '../widgets/PasswordDialog';
 class Payments extends Component {
   constructor(props) {
     super(props);
-    this.state = { error: {}, showPasswordDialog: false };
+    this.state = { error: {}, showPasswordDialog: false, transferType: 'STEEM' };
     this.formFields = {};
     this.formData = {};
   }
@@ -27,8 +27,7 @@ class Payments extends Component {
           this.setState({ error: this.state.error });
         }
         break;
-      case 'steem':
-      case 'sbd': {
+      case 'amount': {
         if (!value) {
           break;
         }
@@ -60,52 +59,36 @@ class Payments extends Component {
 
       if (v.value) {
         this.formData[k] = v.value.trim();
-        if (k === 'sbd' || k === 'steem') {
+        if (k === 'amount') {
           this.formData[k] = parseFloat(v.value.trim()).toFixed(3);
         }
-      } else if (k !== 'sbd' && k !== 'steem') {
+      } else if (k === 'amount') {
         missingData = true;
       }
     });
 
-    if (missingData || (!this.formData.sbd && !this.formData.steem)) {
+    if (missingData || !this.formData.amount) {
       this.state.error.transfer = 'Please fill all fields';
       this.setState({ error: this.state.error });
     } else {
-      this.state.error.transfer = undefined;
-      this.setState({ error: this.state.error });
-
-      console.log(this.formData);
       const { auth: { user } } = this.props;
+      const balance = this.state.transferType === 'STEEM' ? `${this.formData.amount} STEEM` : `${this.formData.amount} SBD`;
 
-      if (this.formData.steem || this.formData.sbd) {
-        this.setState({
-          showPasswordDialog: true,
-          passwordCallback: (passwordOrWif) => {
-            if (this.formData.steem) {
-              console.log('Transfering steem');
-              this.props.transfer({
-                passwordOrWif,
-                from: user.name,
-                balance: `${this.formData.steem} STEEM`,
-                to: this.formData.transferTo,
-                memo: this.formData.memo,
-              });
-            }
-
-            if (this.formData.sbd) {
-              console.log('Transfering sbd');
-              this.props.transfer({
-                passwordOrWif,
-                from: user.name,
-                balance: `${this.formData.sbd} SBD`,
-                to: this.formData.transferTo,
-                memo: this.formData.memo,
-              });
-            }
-          },
-        });
-      }
+      this.state.error.transfer = undefined;
+      this.setState({
+        error: this.state.error,
+        showPasswordDialog: true,
+        passwordCallback: (passwordOrWif) => {
+          console.log('Transfering steem');
+          this.props.transfer({
+            passwordOrWif,
+            from: user.name,
+            balance,
+            to: this.formData.transferTo,
+            memo: this.formData.memo,
+          });
+        },
+      });
     }
   }
 
@@ -116,40 +99,47 @@ class Payments extends Component {
     this.state.passwordCallback(passwordOrWif);
   }
 
-  render() {
-    console.log(this.props.payments);
-    const { auth: { user } } = this.props;
+  changeTransferType = (event) => {
+    this.setState({ transferType: event.target.value });
+  }
 
+  render() {
+    const { auth: { user } } = this.props;
+    const currentBalance = this.state.transferType === 'STEEM' ? user.balance : user.sbd_balance;
     return (
-      <div>
-        <h1 className="mvl">Payments</h1>
-        <form className="form form-payments">
-          <div className="pam">
-            <fieldset className="form-group">
-              <label htmlFor="transferTo">Transfer to</label>
-              <input className="form-control form-control-lg" placeholder="Send to account" onBlur={() => this.validate('transferTo')} ref={c => (this.formFields.transferTo = c)} />
-              <div className="form-control-feedback">{this.state.error.transferTo}</div>
+      <div className="container">
+        <div className="block block-profile mtl mbs">
+          <h1 className="mvl">Payments</h1>
+          <form className="form form-payments">
+            <div className="pam">
+              <fieldset className="form-group">
+                <label htmlFor="transferType">Transfer Steem/Steem Dollar</label>
+                <select className="form-control form-control-lg" value={this.state.transferType} onChange={this.changeTransferType}>
+                  <option>STEEM</option>
+                  <option>SBD</option>
+                </select>
+              </fieldset>
+              <fieldset className="form-group">
+                <label htmlFor="transferTo">Transfer to</label>
+                <input className="form-control form-control-lg" placeholder="Send to account" onBlur={() => this.validate('transferTo')} ref={c => (this.formFields.transferTo = c)} />
+                <div className="form-control-feedback">{this.state.error.transferTo}</div>
+              </fieldset>
+              <fieldset className="form-group">
+                <label htmlFor="steem-dollar">Current balance {currentBalance}</label>
+                <input className="form-control form-control-lg" placeholder="How many SteemDollar to send?" onBlur={() => this.validate('amount')} ref={c => (this.formFields.amount = c)} />
+                <div className="form-control-feedback">{this.state.error.amount}</div>
+              </fieldset>
+              <fieldset className="form-group">
+                <label htmlFor="memo">Memo</label>
+                <input className="form-control form-control-lg" placeholder="add a note to transaction." ref={c => (this.formFields.memo = c)} />
+              </fieldset>
+            </div>
+            <fieldset className="form-group man">
+              <div className="form-control-feedback man phm">{this.state.error.transfer}</div>
+              <button className="btn btn-primary form-submit" onClick={this.transfer}>Transfer</button>
             </fieldset>
-            <fieldset className="form-group">
-              <label htmlFor="Steem">Send Steem (has {user.balance})</label>
-              <input className="form-control form-control-lg" placeholder="How many steem to send?" onBlur={() => this.validate('steem')} ref={c => (this.formFields.steem = c)} />
-              <div className="form-control-feedback">{this.state.error.steem}</div>
-            </fieldset>
-            <fieldset className="form-group">
-              <label htmlFor="steem-dollar">Send SteemDollar (has {user.sbd_balance})</label>
-              <input className="form-control form-control-lg" placeholder="How many SteemDollar to send?" onBlur={() => this.validate('sbd')} ref={c => (this.formFields.sbd = c)} />
-              <div className="form-control-feedback">{this.state.error.sbd}</div>
-            </fieldset>
-            <fieldset className="form-group">
-              <label htmlFor="memo">Memo</label>
-              <input className="form-control form-control-lg" ref={c => (this.formFields.memo = c)} />
-            </fieldset>
-          </div>
-          <fieldset className="form-group man">
-            <div className="form-control-feedback man phm">{this.state.error.transfer}</div>
-            <button className="btn btn-primary form-submit" onClick={this.transfer}>Transfer</button>
-          </fieldset>
-        </form>
+          </form>
+        </div>
         {this.state.showPasswordDialog &&
           <PasswordDialog
             isUpdating={user.isUpdatingProfile}
