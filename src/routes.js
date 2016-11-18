185927af362a7b9d8a27e41160b00d83ba91fab7
adcpm <fabien@bonustrack.co>
2016-11-18 11:37:37 +0100

    Init simple app tutorial page

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import ModelWrapper from './ModelWrapper';
 import Profile from './profile/Profile';
 import Authorize from './auth/Authorize';
 import Developer from './developers/Developers';
+import TutorialsSimpleApp from './developers/Tutorials/TutorialsSimpleApp';
 import Summary from './summary/Summary';
 import Apps from './apps/Apps';
 import App from './apps/App';
@@ -20,6 +21,7 @@ module.exports = (
       <Route path="/apps/@:app" component={App} />
       <Route path="/profile" component={Profile} />
       <Route path="/developers" component={Developer} />
+      <Route path="/developers/tutorials/simple-app" component={TutorialsSimpleApp} />
     </Route>
 
     <Route component={ModelWrapper}>
