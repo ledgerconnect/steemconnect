465f61221b2c773ad48a4470d50c600cc2a61b50
adcpm <fabien@bonustrack.co>
2016-11-27 14:52:57 +0100

    Use steemit styling for vote widget

--- routes/embed.js
+++ routes/embed.js
@@ -6,12 +6,16 @@ const router = new express.Router();
 router.get('/vote', (req, res) => {
   const author = req.query.author || 'siol';
   const permlink = req.query.permlink || 'test-periscope';
+  const weight = req.query.weight || 10000;
+
   steem.api.getContent(author, permlink, (err, result) => {
     res.render('embed/vote', {
       layout: 'embed',
       author,
       permlink,
-      content: JSON.stringify(result),
+      weight,
+      content: result,
+      json: JSON.stringify(result),
     });
   });
 });
