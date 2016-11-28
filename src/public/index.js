de2fe5e518f585ebbf0926d17fff5a34d4ad5c44
adcpm <fabien@bonustrack.co>
2016-11-28 15:21:28 +0100

    Init homepage

--- src/public/index.js
+++ src/public/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default () => <div className="my-3">
-  <h1 className="mb-3">Welcome SteemConnect</h1>
-  <div className="row pvl thin text-lg-left">
-    <div className="pvl">
+export default () => <div className="splash">
+  <div className="splash__header">
+    <div className="container">
+      <img src="/img/logo.svg" height="60" />
+    </div>
+  </div>
+  <div className="splash__body my-3">
+    <div className="container-fluid">
+      <h1>Build apps on Steem blockchain</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Nulla sed porta metus, eu efficitur sapien.</p>
       <Link to="/login" className="btn btn-primary">Login</Link>
     </div>
-    <div className="pvl">
-      <Link to="/register" className="btn btn-primary">Register</Link>
+  </div>
+  <hr />
+  <div className="container">
+    <div className="my-3">
+      <i className="icon icon-xl material-icons">perm_identity</i>
+    </div>
+    <div className="my-3">
+      <i className="icon icon-xl material-icons">apps</i>
+    </div>
+    <div className="my-3">
+      <i className="icon icon-xl material-icons">code</i>
     </div>
   </div>
 </div>;
