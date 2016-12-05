68ad175846c7db22ab2048e53d366ecaf8fe3f55
mynameisek <mynameiskeo@gmail.com>
2016-12-05 05:39:07 +0100

    Improved landing Txt SC

--- src/public/index.js
+++ src/public/index.js
@@ -9,28 +9,25 @@ export default () => <div className="splash">
   </div>
   <div className="splash__body my-3 py-3">
     <div className="container">
-      <h1>Identity management for Steem</h1>
-      <p>This simple identity layer built on top of the Steem
-        blockchain accounts allows Apps to verify your identity
-        based on the authentication performed by Steem Connect
-        without storing your password.</p>
+      <h1>Identity, authentication, authorization for Steem blockchain’s apps</h1>
+      <p>SteemConnect is a simple identity layer built on top of the Steem blockchain accounts that allows you to connect to authorized apps is a secure and convenient way.
+</p>
       <Link to="/login" className="btn btn-primary">Login</Link>
     </div>
   </div>
   <hr />
   <div className="container my-3">
     <div className="thin my-3">
-      <h2 className="py-3">Lorem ipsum dolor sit amet</h2>
+      <h2 className="py-3">SteemConnect Use cases</h2>
       <div className="my-3 text-md-left">
         <div className="row">
           <div className="col col-md-2">
             <i className="icon icon-xl material-icons">perm_identity</i>
           </div>
           <div className="col col-md-10">
-            <h3>Zero-friction authentication and authorization</h3>
-            <p>Access to authorized Website and applications with your
-              Steem account credentials using mechanisms for robust
-              signing and encryption without giving your password.</p>
+            <h3>Secure login to authorized Steem apps</h3>
+            <p>Access to authorized websites and apps with your Steem account credentials using mechanisms for robust signing and encryption without giving to secure your password/key 
+</p>
           </div>
         </div>
       </div>
@@ -41,9 +38,8 @@ export default () => <div className="splash">
               <i className="icon icon-xl material-icons">apps</i>
             </div>
             <div className="col col-md-10">
-              <h3>Steem App Store</h3>
-              <p>Access to authorized website and application with
-                Steem Connect using your Steem credentials with confidence.</p>
+              <h3>SteemConnect App Store</h3>
+              <p>Select among various websites and apps using SteemConnect where you can easily log in with confidence</p>
             </div>
           </div>
         </div>
@@ -57,7 +53,7 @@ export default () => <div className="splash">
             <div className="col col-md-10">
               <h3>Light Wallet, Activity & Profile Manager</h3>
               <p>Manage your wallet, check your activity on Steem
-                and edit your global Profile data.</p>
+                and edit your global Steem Profile</p>
             </div>
           </div>
         </div>
