738e87432dd933d81e7d671adb13a00b2ba03191
adcpm <fabien@bonustrack.co>
2017-01-19 22:10:25 +0700

    Init query parsing

--- src/helpers/operationHelpers.js
+++ src/helpers/operationHelpers.js
@@ -1,5 +1,7 @@
-import operations from 'steem/lib/broadcast/operations.json';
+import { formatter } from 'steem';
 import changeCase from 'change-case';
+import diacritics from 'diacritics';
+import operations from 'steem/lib/broadcast/operations.json';
 
 export const getOperation = type => {
   const ops = operations.filter(op =>
@@ -19,4 +21,37 @@ export const isValid = (op, params) => {
     });
   }
   return isValid;
+};
+
+export const parseQuery = (type, query, username) => {
+  type = changeCase.snakeCase(type);
+  switch (type) {
+    case 'vote':
+      return parseVote(query, username);
+    case 'comment':
+      return parseComment(query, username);
+    default:
+      return query;
+  }
+};
+
+export const parseVote = (query, username) => {
+  query.voter = query.voter || username;
+  query.weight = query.weight || 10000;
+  return query;
+};
+
+export const parseComment = (query, username) => {
+  query.author = query.author || username;
+  if (query.parent_author && query.parent_permlink) {
+    query.title = query.title || '';
+    query.permlink = query.permlink
+      || formatter.commentPermlink(query.parent_author, query.parent_permlink).toLowerCase()
+  } else {
+    query.title = query.title || query.body.slice(0, 255);
+    query.permlink = query.permlink
+      || changeCase.paramCase(diacritics.remove(query.title)).slice(0, 255)
+  }
+  query.json_metadata = query.json_metadata || '';
+  return query;
 };
