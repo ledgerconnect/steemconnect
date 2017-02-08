07e3148e8a9b9bf1003bc0de1dc4d0877ec6e8b0
adcpm <fabien@bonustrack.co>
2017-02-08 20:53:36 +0700

    Improve error layout

--- src/sign/SignError.js
+++ src/sign/SignError.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Icon from '../widgets/Icon';
 
 const SignError = ({ error, resetForm }) =>
   <div>
-    <h4 className="text-danger">Oops, something goes wrong!</h4>
+    <h2><Icon name="close" className="text-danger" lg /> Error</h2>
+    <h5>Oops, something goes wrong!</h5>
     {error.code && error.data &&
-      <h5><strong>Error code: {error.code}</strong> {error.data.message}</h5>
+      <p><b>Error code: {error.code}</b> {error.data.message}</p>
     }
     <p>Do you want to <a href="#" onClick={() => resetForm()}>try again</a>?</p>
   </div>;
