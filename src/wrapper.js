0960d2742dde3b1c6a67873182d3a57e1647e784
adcpm <fabien@bonustrack.co>
2017-02-17 14:20:35 +0700

    Fix default websocket

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,5 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import steem from 'steem';
 import './App.scss';
+
+steem.api.setWebSocket(process.env.WS);
 
 export default class Wrapper extends Component {
   render() {
