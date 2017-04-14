a394590be5b98a071655541a12f8c530a0d41148
adcpm <fabien@bonustrack.co>
2017-04-14 21:22:00 +0700

    Remove steem.setWebSocket() call (depreciated)

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,8 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import steem from 'steem';
 import './App.scss';
-
-steem.api.setWebSocket(process.env.WS);
 
 export default class Wrapper extends Component {
   render() {
