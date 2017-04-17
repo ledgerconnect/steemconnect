3c1eef29ab6a279e35bb0218001e83ffbc833c57
adcpm <fabien@bonustrack.co>
2017-04-18 01:48:21 +0700

    Init route /me and improve broadcast route

--- routes/api.js
+++ routes/api.js
@@ -1,42 +1,32 @@
 const express = require('express');
+const { authenticateApp, verifyPermissions } = require('../helpers/middleware');
 const router = express.Router();
 
-router.get('/test', (req, res, next) => {
-  const operations = [['vote', {
-    voter: 'guest123',
-    author: 'krnel',
-    permlink: 'getting-too-personal-with-ai-assistants',
-    weight: 10000,
-  }]];
-
-  res.steem.broadcast.send(
-    { operations, extensions: [] },
-    { posting: process.env.BROADCASTER_POSTING_WIF },
-    (err, result) => {
-      console.log(err, result);
-      res.json({ errors: err, result });
-    }
-  );
+/** Get user account details */
+router.all('/me', authenticateApp, verifyPermissions, async (req, res, next) => {
+  const accounts = await res.steem.api.getAccountsAsync([req.user]);
+  res.json(accounts[0]);
 });
 
-router.post('/rpc', (req, res, next) => {
-  const { operations } = req.body;
+/** Broadcast transactions */
+router.post('/broadcast', authenticateApp, verifyPermissions, async (req, res, next) => {
   const allowedOperations = ['comment', 'comment_options', 'vote']; // custom_json
-  const errors = [];
+  const { operations } = req.body;
+  let isAuthorized = true;
 
-  /** Check if operations are allowed */
   operations.forEach((operation) => {
+    /** Check if operation is allowed */
     if (allowedOperations.indexOf(operation[0]) === -1) {
-      errors.push(`Operation '${operation[0]}' is not allowed.`);
+      isAuthorized = false;
     }
-    /** Restrict the use to guest123 account */
-    if (operation[1].voter !== 'guest123' && operation[1].author !== 'guest123') {
-      errors.push(`You are not allowed to perform operation for '${operation[1].voter || operation[0].author}'`);
+    /** Check if author of the operation is user */
+    if (operation[1].voter !== req.user && operation[1].author !== req.user) {
+      isAuthorized = false;
     }
   });
 
-  if (errors.length > 0) {
-    res.json({ errors });
+  if (!isAuthorized) {
+    res.status(401).send('Unauthorized!');
   } else {
     res.steem.broadcast.send(
       { operations, extensions: [] },
