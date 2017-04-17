a8ea65af859408c12b993421fe98ed518a8720ea
adcpm <fabien@bonustrack.co>
2017-04-18 01:45:09 +0700

    Init login page

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Wrapper from './wrapper';
 import Index from './statics/Index';
+import Docs from './statics/Docs';
 import Sign from './sign/Sign';
-import Permissions from './auth/Permissions'
+import Login from './auth/Login'
+import Permissions from './auth/Permissions';
 import Authorize from './auth/Authorize';
 import Revoke from './auth/Revoke';
 
 export default (
   <Route path="/" component={Wrapper}>
     <IndexRoute component={Index} />
+    <Route path="/docs" component={Docs} />
+    <Route path="/login" component={Login} />
     <Route path="/sign/:type" component={Sign} />
     <Route path="/@:username/permissions" component={Permissions} />
     <Route path="/authorize/@:username" component={Authorize} />
