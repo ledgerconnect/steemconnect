b04cdaff4a0b3519642d3896ba41b873c6cbc688
adcpm <fabien@bonustrack.co>
2017-04-18 01:44:15 +0700

    Move documentation on /docs page and show SteemConnect on index

--- src/statics/Index.js
+++ src/statics/Index.js
@@ -1,51 +1,9 @@
 import React from 'react';
-import changeCase from 'change-case';
-import methods from 'steem/lib/api/methods.json';
-import operations from 'steem/lib/broadcast/operations.json';
-
-const Method = ({ method }) => {
-  const inlineParams = method.params
-    ? method.params.join(', ') + ', ' :
-    '';
-  return (
-    <div className="mb-4">
-      <h5>{changeCase.titleCase(method.method)}</h5>
-      <pre>
-        {`steem.api.${changeCase.camelCase(method.method)}(${inlineParams}function(err, result) {
-  console.log(err, result);
-});`}
-      </pre>
-    </div>
-  );
-};
-
-const Operation = ({ operation }) => {
-  const inlineParams = operation.params
-    ? operation.params.map((param) => changeCase.camelCase(param)).join(', ') + ', '
-    : '';
-  return (
-    <div className="mb-4">
-      <h5>{changeCase.titleCase(operation.operation)}</h5>
-      <pre>
-        {`steem.broadcast.${changeCase.camelCase(operation.operation)}(wif, ${inlineParams}function(err, result) {
-  console.log(err, result);
-});`}
-      </pre>
-    </div>
-  )
-};
 
 const Index = () => {
   return (
-    <div className="container my-5">
-      <h2>API</h2>
-      {methods.map((method, key) =>
-        <Method key={key} method={method} />
-      )}
-      <h2>Broadcast</h2>
-      {operations.map((operation, key) =>
-        <Operation key={key} operation={operation} />
-      )}
+    <div className="Sign">
+      <h1>SteemConnect</h1>
     </div>
   );
 };
