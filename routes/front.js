24e715a5a12e626c071afcdebf286c7a38e7f4f0
adcpm <fabien@bonustrack.co>
2017-04-19 22:33:17 +0700

    Change homepage title

--- routes/front.js
+++ routes/front.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/*', (req, res, next) => {
-  res.render('index', { title: 'Steem.js' });
+  res.render('index', { title: 'SteemConnect' });
 });
 
 module.exports = router;
