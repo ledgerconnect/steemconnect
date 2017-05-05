cc2e7de34731f71e21d9a60c2c354d6f79c69459
adcpm <fabien@bonustrack.co>
2017-05-05 21:51:36 +0700

    Change dashboard links

--- src/statics/Dashboard.js
+++ src/statics/Dashboard.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router';
 const Dashboard = ({ app }) => {
   return (
     <div className="container my-5">
-      <h4>My Account</h4>
-      <p><Link to={`/@${app.user.name}/permissions`}>Permissions</Link></p>
-      <p><Link to={`/apps`}>Apps</Link></p>
+      <h4>Applications</h4>
+      <p><Link to="/apps">Apps</Link></p>
+      <p><Link to="/apps/authorized">Authorized Apps</Link></p>
       <h4>Developers</h4>
-      <p><Link to={`/apps/me`}>My Apps</Link></p>
+      <p><Link to="/apps/me">My Apps</Link></p>
     </div>
   );
 };
