e332575165a46b08127eede1801755e191407825
adcpm <fabien@bonustrack.co>
2017-05-07 03:26:04 +0700

    Refactor

--- src/apps/App.js
+++ src/apps/App.js
@@ -41,48 +41,50 @@ export default class MyApps extends Component {
     const { app, clientId, isLoading, isLoaded, revealSecret } = this.state;
     return (
       <div className="container my-5">
-        <Avatar username={clientId} size="80" className="float-left mr-3" />
-        <h2 className="d-inline">{clientId}</h2>
-        <span className="float-right">
-          {isLoaded && app.admins.includes(this.props.app.user.name) &&
-            <Link to={`/apps/@${clientId}/edit`} className="btn btn-secondary btn-sm ml-2">
-              Edit
-            </Link>
-          }
-          {this.props.app.isAuthenticated && hasAuthority(this.props.app.user, clientId) &&
-            <Link to={`/revoke/@${clientId}`} className="btn btn-danger btn-sm ml-2">
-              Revoke
-            </Link>
-          }
-        </span>
-        <p>@{clientId}</p>
-        {isLoading && <Loading/>}
         {isLoaded &&
-          <div className=" pt-4">
-            <p>This application is secured by SteemConnect.</p>
-            <div className="list-group">
-              <div className="list-group-item">
-                <b className="mr-1">Client Id:</b> {app.client_id}
-              </div>
-              {app.secret &&
-                <div className="list-group-item">
-                  <b className="mr-1">Client Secret:</b>
-                  {revealSecret
-                    ? <div>
-                      {`${app.secret} `}
-                      <a href="#" onClick={() => this.setState({ revealSecret: false })}>
-                        hide
-                      </a>
-                    </div>
-                    : <a href="#" onClick={() => this.setState({ revealSecret: true })}>
-                      click to reveal
-                    </a>
-                }
-                </div>
+          <div>
+            <Avatar icon={app.icon} size="80" className="float-left mr-3" />
+            <h2 className="d-inline">{clientId}</h2>
+            <span className="float-right">
+              {isLoaded && app.admins.includes(this.props.app.user.name) &&
+                <Link to={`/apps/@${clientId}/edit`} className="btn btn-secondary btn-sm ml-2">
+                  Edit
+                </Link>
               }
+              {this.props.app.isAuthenticated && hasAuthority(this.props.app.user, clientId) &&
+                <Link to={`/revoke/@${clientId}`} className="btn btn-danger btn-sm ml-2">
+                  Revoke
+                </Link>
+              }
+            </span>
+            <p>@{clientId}</p>
+            <div className="pt-4">
+              <p>This application is secured by SteemConnect.</p>
+              <div className="list-group">
+                <div className="list-group-item">
+                  <b className="mr-1">Client Id:</b> {app.client_id}
+                </div>
+                {app.secret &&
+                  <div className="list-group-item">
+                    <b className="mr-1">Client Secret:</b>
+                    {revealSecret
+                      ? <div>
+                        {`${app.secret} `}
+                        <a href="#" onClick={() => this.setState({ revealSecret: false })}>
+                          hide
+                        </a>
+                      </div>
+                      : <a href="#" onClick={() => this.setState({ revealSecret: true })}>
+                        click to reveal
+                      </a>
+                    }
+                  </div>
+                }
+              </div>
             </div>
           </div>
         }
+        {isLoading && <Loading/>}
       </div>
     );
   }
