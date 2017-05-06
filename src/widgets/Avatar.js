890a43a3cac2b10e1b9b3965e5402032d786268b
adcpm <fabien@bonustrack.co>
2017-05-07 03:25:08 +0700

    Make avatar works with url

--- src/widgets/Avatar.js
+++ src/widgets/Avatar.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './Avatar.scss';
 
-const Icon = ({
+const Avatar = ({
   username,
+  icon,
   size = '36',
   className = '',
 }) => {
+  const src = username
+    ? `https://img.steemconnect.com/@${username}?s=${size}`
+    : icon
+      ? `https://steemitimages.com/${size}x${size}/${icon}`
+      : `https://img.steemconnect.com/@steemconnect?s=${size}`;
   return (
     <span
       className={`Avatar ${className}`}
       style={{ height: `${size}px`, width: `${size}px` }}
     >
-      <img src={`https://img.steemconnect.com/@${username}?s=${size}`}/>
+      <img src={src}/>
     </span>
   );
 };
 
-export default Icon;
+export default Avatar;
