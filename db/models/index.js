f97db6ad48b4efcbd2f11f5290767af8063425fc
adcpm <fabien@bonustrack.co>
2017-05-17 00:51:54 +0700

    Add config with env variable

--- db/models/index.js
+++ db/models/index.js
@@ -9,7 +9,7 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
