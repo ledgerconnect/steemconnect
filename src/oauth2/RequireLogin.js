590d7050ddaf71c988ee57ab93466ac73b572125
adcpm <fabien@bonustrack.co>
2017-05-31 00:46:39 +0700

    Redirect unlogged user to login page

--- src/oauth2/RequireLogin.js
+++ src/oauth2/RequireLogin.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 import Loading from '../widgets/Loading';
 
-const RequireLogin = (props) =>
-  (props.app.isLoaded && props.app.user.name)
-    ? React.cloneElement(props.children, { app: props.app })
+const RequireLogin = (props) => {
+  if (props.app.isLoaded && !props.app.user.name) {
+    const to = next === '/dashboard' ? '/login' : `/login?next=${next}`;
+    browserHistory.push(to);
+  }
+  return (props.app.isLoaded && props.app.user.name)
+    ? React.cloneElement(props.children, {app: props.app})
     : <div className="Sign">
-        <div className="Sign__content container my-2">
-          {props.app.isLoading
-            ? <Loading/>
-            : <p>You must log in to do that</p>
-          }
-        </div>
+      <div className="Sign__content container my-2">
+        {props.app.isLoading && <Loading/>}
+      </div>
     </div>;
+};
 
 export default RequireLogin;
