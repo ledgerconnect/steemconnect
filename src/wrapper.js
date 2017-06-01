9117c43dc3672e03f8995b9242e84a6ef0663c9b
adcpm <fabien@bonustrack.co>
2017-06-01 15:11:56 +0700

    Import antd with less and setup custom variable

--- src/wrapper.js
+++ src/wrapper.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { authenticate } from './actions/auth';
-import './App.scss';
+import './styles/common.less';
 
 @connect(
   state => ({
