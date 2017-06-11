1be1da631627453ac15e7aec6a567fd44fc85cfb
adcpm <fabien@bonustrack.co>
2017-06-11 15:14:56 +0700

    Improve oauth2 doc

--- src/components/Docs/OAuth2.js
+++ src/components/Docs/OAuth2.js
@@ -7,11 +7,28 @@ const OAuth2 = ({ app }) => {
     <div className="container my-5">
       <h3>OAuth2</h3>
       <h4>Implementing OAuth2</h4>
-      <p>The URLs for OAuth2 are as follows:</p>
+      <p>
+        The first step in implementing OAuth2 is <Link to="/apps/create">registering a developer application</Link>
+        , and retrieving your client ID and client secret. The URLs for OAuth2 are as follows:
+      </p>
       <div className="list-group">
         <div className="list-group-item">
-          {`${origin}/api/oauth2/authorize`}
-          {` Base authorization URL`}
+          <a className="mr-2" href={`${origin}/api/oauth2/authorize`}>
+            {`${origin}/api/oauth2/authorize`}
+          </a>
+          <b>Base authorization URL</b>
+        </div>
+        <div className="list-group-item">
+          <a className="mr-2" href={`${origin}/api/oauth2/token`}>
+            {`${origin}/api/oauth2/token`}
+          </a>
+          <b>Token URL</b>
+        </div>
+        <div className="list-group-item">
+          <a className="mr-2" href={`${origin}/api/oauth2/token/revoke`}>
+            {`${origin}/api/oauth2/token/revoke`}
+          </a>
+          <b>Revocation URL</b>
         </div>
       </div>
     </div>
