a380f6724a137aab338d8938521f0c426e081ee0
adcpm <fabien@bonustrack.co>
2017-06-17 01:45:54 +0700

    Add validators accountNotExist and validateAccountName

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -16,3 +16,53 @@ export const accountExist = (rule, value, callback) => {
     }
   });
 };
+
+export const accountNotExist = (rule, value, callback) => {
+  steem.api.getAccounts([value], (err, result) => {
+    if (result[0]) {
+      callback(['Account name is not available']);
+    } else {
+      callback();
+    }
+  });
+};
+
+// https://github.com/steemit/condenser/blob/eaf8a02658b8deaef376ec90b81d0866e52582cc/app/utils/ChainValidation.js#L4
+export const validateAccountName = (rule, value, callback) => {
+  let i, label, len, length, ref, suffix;
+
+  suffix = 'Account name should ';
+  if (!value) {
+    callback([suffix + 'not be empty.']);
+  }
+  length = value.length;
+  if (length < 3) {
+    callback([suffix + 'be longer.']);
+  }
+  if (length > 16) {
+    callback([suffix + 'be shorter.']);
+  }
+  if (/\./.test(value)) {
+    suffix = 'Each account segment should ';
+  }
+  ref = value.split('.');
+  for (i = 0, len = ref.length; i < len; i++) {
+    label = ref[i];
+    if (!/^[a-z]/.test(label)) {
+      callback([suffix + 'start with a letter.']);
+    }
+    if (!/^[a-z0-9-]*$/.test(label)) {
+      callback([suffix + 'have only letters, digits, or dashes.']);
+    }
+    if (/--/.test(label)) {
+      callback([suffix + 'have only one dash in a row.']);
+    }
+    if (!/[a-z0-9]$/.test(label)) {
+      callback([suffix + 'end with a letter or digit.']);
+    }
+    if (!(label.length >= 3)) {
+      callback([suffix + 'be longer']);
+    }
+  }
+  callback();
+};
