54fe7c0f7c5ceab86291968b4d13bfbeaac4c870
Nilesh Suthar <nileshsuthar@live.in>
2017-06-26 11:49:13 +0530

    Refactor

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,145 @@ function sendResponse({ err: error, result }, res) {
   }
   return res.json({ result });
 }
+function verify(req, res) {
+  const token = jwt.sign(
+    {
+      userId: req.userId,
+      username: req.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '1 day',
+    }
+  );
+
+  if (req.username && (req.permissions !== null || req.baseUrl === '/api/@steemconnect')) {
+    return res.json({
+      userId: req.userId,
+      isAuthenticated: true,
+      username: req.username,
+      permissions: req.permissions,
+      token,
+    });
+  }
+  return res.json({
+    isAuthenticated: false,
+  });
+}
+function vote({ voter, author, permlink, weight }, req, res) {
+  weight = parseInt(weight, 10);
+  steem.broadcast.vote(req.wif, voter, author, permlink, weight, (err, result) =>
+    sendResponse({ err, result }, res)
+  );
+}
+function comment(
+  { parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata },
+  req,
+  res
+) {
+  steem.broadcast.comment(
+    req.wif,
+    parentAuthor,
+    parentPermlink,
+    author,
+    permlink,
+    title,
+    body,
+    jsonMetadata,
+    (err, result) => sendResponse({ err, result }, res)
+  );
+}
+
+function deleteComment({ author, permlink }, req, res) {
+  steem.broadcast.comment(req.wif, author, permlink, (err, result) =>
+    sendResponse({ err, result }, res)
+  );
+}
+function reblog({ permlink, account, author }, req, res) {
+  const requiredAuths = [];
+  const requiredPostingAuths = [req.username];
+  const id = 'follow';
+  const json = JSON.stringify(['reblog', { account, author, permlink }]);
+  steem.broadcast.customJson(
+    req.wif,
+    requiredAuths,
+    requiredPostingAuths,
+    id,
+    json,
+    (err, result) => sendResponse({ err, result }, res)
+  );
+}
+function follow({ follower, following }, req, res) {
+  const requiredAuths = [];
+  const requiredPostingAuths = [req.username];
+  const id = 'follow';
+  const json = JSON.stringify(['follow', { follower, following, what: ['blog'] }]);
+  steem.broadcast.customJson(
+    req.wif,
+    requiredAuths,
+    requiredPostingAuths,
+    id,
+    json,
+    (err, result) => sendResponse({ err, result }, res)
+  );
+}
+function unfollow({ unfollower, unfollowing }, req, res) {
+  const requiredAuths = [];
+  const requiredPostingAuths = [req.username];
+  const id = 'follow';
+  const json = JSON.stringify([
+    'follow',
+    { follower: unfollower, following: unfollowing, what: [] },
+  ]);
+  steem.broadcast.customJson(
+    req.wif,
+    requiredAuths,
+    requiredPostingAuths,
+    id,
+    json,
+    (err, result) => sendResponse({ err, result }, res)
+  );
+}
+function ignore({ follower, following }, req, res) {
+  const requiredAuths = [];
+  const requiredPostingAuths = [req.username];
+  const id = 'follow';
+  const json = JSON.stringify(['follow', { follower, following, what: ['ignore'] }]);
+  steem.broadcast.customJson(
+    req.wif,
+    requiredAuths,
+    requiredPostingAuths,
+    id,
+    json,
+    (err, result) => sendResponse({ err, result }, res)
+  );
+}
+
+function customJson({ id, json, requiredPostingAuths, requiredAuths }, req, res) {
+  steem.broadcast.customJson(
+    req.wif,
+    requiredAuths,
+    requiredPostingAuths,
+    id,
+    json,
+    (err, result) => sendResponse({ err, result }, res)
+  );
+}
+
+function claimRewardBalance({ account, rewardSteem, rewardSbd, rewardVests }, req, res) {
+  if (req.username !== account) {
+    sendResponse({ err: 'account name must match with steemconnect account name' }, res);
+  } else {
+    steem.broadcast.claimRewardBalance(
+      req.wif,
+      account,
+      rewardSteem,
+      rewardSbd,
+      rewardVests,
+      (err, result) => sendResponse({ err, result }, res)
+    );
+  }
+}
 
 router.get('/api/authorize', (req, res) => {
   res.redirect(302, `/authorize?${querystring.stringify(req.query)}`);
@@ -32,309 +171,28 @@ router.get('/api/verifyToken', (req, res) => {
 });
 
 router.use('/api', verifyAuth);
-
 router.use('/api/@:appUserName', checkOrigin, checkPermission, apiRouter);
 
-apiRouter.get('/verify', (req, res) => {
-  const token = jwt.sign(
-    {
-      userId: req.userId,
-      username: req.username,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: '1 day',
-    }
-  );
+apiRouter.get('/verify', verify);
+apiRouter.get('/vote', (req, res) => vote(req.query, req, res));
+apiRouter.get('/comment', (req, res) => comment(req.query, req, res));
+apiRouter.get('/deleteComment', (req, res) => deleteComment(req.query, req, res));
+apiRouter.get('/reblog', (req, res) => reblog(req.query, req, res));
+apiRouter.get('/follow', (req, res) => follow(req.query, req, res));
+apiRouter.get('/unfollow', (req, res) => unfollow(req.query, req, res));
+apiRouter.get('/ignore', (req, res) => ignore(req.query, req, res));
+apiRouter.get('/customJson', (req, res) => customJson(req.query, req, res));
+apiRouter.get('/claimRewardBalance', (req, res) => claimRewardBalance(req.query, req, res));
 
-  if (req.username && (req.permissions !== null || req.baseUrl === '/api/@steemconnect')) {
-    return res.json({
-      userId: req.userId,
-      isAuthenticated: true,
-      username: req.username,
-      permissions: req.permissions,
-      token,
-    });
-  }
-  return res.json({
-    isAuthenticated: false,
-  });
-});
-
-apiRouter.get('/vote', (req, res) => {
-  const { voter, author, permlink } = req.query;
-  const weight = parseInt(req.query.weight, 10);
-  steem.broadcast.vote(req.wif, voter, author, permlink, weight, (err, result) =>
-    sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.get('/comment', (req, res) => {
-  const { parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata } = req.query;
-  steem.broadcast.comment(
-    req.wif,
-    parentAuthor,
-    parentPermlink,
-    author,
-    permlink,
-    title,
-    body,
-    jsonMetadata,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.get('/deleteComment', (req, res) => {
-  const { author, permlink } = req.query;
-  steem.broadcast.comment(req.wif, author, permlink, (err, result) =>
-    sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.get('/reblog', (req, res) => {
-  const { permlink, account, author } = req.query;
-  const requiredAuths = [];
-  const requiredPostingAuths = [req.username];
-  const id = 'follow';
-  const json = JSON.stringify(['reblog', { account, author, permlink }]);
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.get('/follow', (req, res) => {
-  const { follower, following } = req.query;
-  const requiredAuths = [];
-  const requiredPostingAuths = [req.username];
-  const id = 'follow';
-  const json = JSON.stringify(['follow', { follower, following, what: ['blog'] }]);
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.get('/unfollow', (req, res) => {
-  const { unfollower, unfollowing } = req.query;
-  const requiredAuths = [];
-  const requiredPostingAuths = [req.username];
-  const id = 'follow';
-  const json = JSON.stringify([
-    'follow',
-    { follower: unfollower, following: unfollowing, what: [] },
-  ]);
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.get('/ignore', (req, res) => {
-  const { follower, following } = req.query;
-  const requiredAuths = [];
-  const requiredPostingAuths = [req.username];
-  const id = 'follow';
-  const json = JSON.stringify(['follow', { follower, following, what: ['ignore'] }]);
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.get('/customJson', (req, res) => {
-  const { id, json, requiredPostingAuths, requiredAuths } = req.query;
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.get('/claimRewardBalance', (req, res) => {
-  const { account, rewardSteem, rewardSbd, rewardVests } = req.query;
-  if (req.username !== account) {
-    sendResponse({ err: 'account name must match with steemconnect account name' }, res);
-  } else {
-    steem.broadcast.claimRewardBalance(
-      req.wif,
-      account,
-      rewardSteem,
-      rewardSbd,
-      rewardVests,
-      (err, result) => sendResponse({ err, result }, res)
-    );
-  }
-});
-
-apiRouter.post('/verify', (req, res) => {
-  const token = jwt.sign(
-    {
-      userId: req.userId,
-      username: req.username,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: '1 day',
-    }
-  );
-
-  if (req.username && (req.permissions !== null || req.baseUrl === '/api/@steemconnect')) {
-    return res.json({
-      userId: req.userId,
-      isAuthenticated: true,
-      username: req.username,
-      permissions: req.permissions,
-      token,
-    });
-  }
-  return res.json({
-    isAuthenticated: false,
-  });
-});
-
-apiRouter.post('/vote', (req, res) => {
-  const { voter, author, permlink } = req.body;
-  const weight = parseInt(req.body.weight, 10);
-  steem.broadcast.vote(req.wif, voter, author, permlink, weight, (err, result) =>
-    sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.post('/comment', (req, res) => {
-  const { parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata } = req.body;
-  steem.broadcast.comment(
-    req.wif,
-    parentAuthor,
-    parentPermlink,
-    author,
-    permlink,
-    title,
-    body,
-    jsonMetadata,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.post('/deleteComment', (req, res) => {
-  const { author, permlink } = req.body;
-  steem.broadcast.comment(req.wif, author, permlink, (err, result) =>
-    sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.post('/reblog', (req, res) => {
-  const { permlink, account, author } = req.body;
-  const requiredAuths = [];
-  const requiredPostingAuths = [req.username];
-  const id = 'follow';
-  const json = JSON.stringify(['reblog', { account, author, permlink }]);
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.post('/follow', (req, res) => {
-  const { follower, following } = req.body;
-  const requiredAuths = [];
-  const requiredPostingAuths = [req.username];
-  const id = 'follow';
-  const json = JSON.stringify(['follow', { follower, following, what: ['blog'] }]);
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.post('/unfollow', (req, res) => {
-  const { unfollower, unfollowing } = req.body;
-  const requiredAuths = [];
-  const requiredPostingAuths = [req.username];
-  const id = 'follow';
-  const json = JSON.stringify([
-    'follow',
-    { follower: unfollower, following: unfollowing, what: [] },
-  ]);
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.post('/ignore', (req, res) => {
-  const { follower, following } = req.body;
-  const requiredAuths = [];
-  const requiredPostingAuths = [req.username];
-  const id = 'follow';
-  const json = JSON.stringify(['follow', { follower, following, what: ['ignore'] }]);
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.post('/customJson', (req, res) => {
-  const { id, json, requiredPostingAuths, requiredAuths } = req.body;
-  steem.broadcast.customJson(
-    req.wif,
-    requiredAuths,
-    requiredPostingAuths,
-    id,
-    json,
-    (err, result) => sendResponse({ err, result }, res)
-  );
-});
-
-apiRouter.post('/claimRewardBalance', (req, res) => {
-  const { account, rewardSteem, rewardSbd, rewardVests } = req.body;
-  if (req.username !== account) {
-    sendResponse({ err: 'account name must match with steemconnect account name' }, res);
-  } else {
-    steem.broadcast.claimRewardBalance(
-      req.wif,
-      account,
-      rewardSteem,
-      rewardSbd,
-      rewardVests,
-      (err, result) => sendResponse({ err, result }, res)
-    );
-  }
-});
+apiRouter.post('/verify', verify);
+apiRouter.post('/vote', (req, res) => vote(req.body, req, res));
+apiRouter.post('/comment', (req, res) => comment(req.body, req, res));
+apiRouter.post('/deleteComment', (req, res) => deleteComment(req.body, req, res));
+apiRouter.post('/reblog', (req, res) => reblog(req.body, req, res));
+apiRouter.post('/follow', (req, res) => follow(req.body, req, res));
+apiRouter.post('/unfollow', (req, res) => unfollow(req.body, req, res));
+apiRouter.post('/ignore', (req, res) => ignore(req.body, req, res));
+apiRouter.post('/customJson', (req, res) => customJson(req.body, req, res));
+apiRouter.post('/claimRewardBalance', (req, res) => claimRewardBalance(req.body, req, res));
 
 module.exports = router;
