8ead05dedbb75c0370e44bb376e341e97b6cd98e
adcpm <fabien@bonustrack.co>
2017-07-11 14:46:55 +0700

    Add comment feature on demo

--- public/demo/app.js
+++ public/demo/app.js
@@ -10,6 +10,9 @@ angular.module('app', [])
     $locationProvider.html5Mode(true);
   }])
   .controller('Main', function($scope, $location, $http) {
+    $scope.loading = false;
+    $scope.parentAuthor = 'siol';
+    $scope.parentPermlink = '5vdmjq-test';
     $scope.accessToken = $location.search().access_token;
     $scope.expiresIn = $location.search().expires_in;
     $scope.loginURL = sc2.getLoginURL();
@@ -25,13 +28,44 @@ angular.module('app', [])
       });
     }
 
-    this.vote = function() {
-      sc2.vote($scope.user.name, 'siol', 'test', 100, function (err, result) {
-        console.log('You successfully vote for @siol/test', err, result);
+    $scope.isAuth = function() {
+      return !!$scope.user;
+    };
+
+    $scope.loadComments = function() {
+      steem.api.getContentReplies($scope.parentAuthor, $scope.parentPermlink, function(err, result) {
+        if (!err) {
+          $scope.comments = result.slice(-5);
+          $scope.$apply();
+        }
       });
     };
 
-    this.logout = function() {
+    $scope.comment = function() {
+      $scope.loading = true;
+      var permlink = steem.formatter.commentPermlink($scope.parentAuthor, $scope.parentPermlink);
+      sc2.comment($scope.parentAuthor, $scope.parentPermlink, $scope.user.name, permlink, '', $scope.message, '', function(err, result) {
+        console.log(err, result);
+        $scope.message = '';
+        $scope.loading = false;
+        $scope.$apply();
+        $scope.loadComments();
+      });
+    };
+
+    $scope.vote = function(author, permlink, weight) {
+      sc2.vote($scope.user.name, author, permlink, weight, function (err, result) {
+        if (!err) {
+          alert('You successfully vote for @' + author + '/' + permlink);
+          console.log('You successfully vote for @siol/test', err, result);
+          $scope.loadComments();
+        } else {
+          console.log(err);
+        }
+      });
+    };
+
+    $scope.logout = function() {
       sc2.revokeToken(function (err, result) {
         console.log('You successfully logged out', err, result);
         delete $scope.user;
