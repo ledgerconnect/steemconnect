a47ff0e7964afee00713eb00ae3814016cd41667
adcpm <fabien@bonustrack.co>
2017-07-15 01:05:33 +0700

    Init metadata helper

---
+++ helpers/metadata.js
@@ -0,0 +1,39 @@
+const { metadata } = require('../db/models');
+
+/** Get user_metadata */
+const getUserMetadata = async (proxy, user) => {
+  try {
+    const userMetadata = await metadata.findOne({ where: { client_id: proxy, user }});
+    if (userMetadata) {
+      return userMetadata.user_metadata;
+    } else {
+      return {};
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/** Update user_metadata */
+const updateUserMetadata = async (proxy, user, newMetadata) => {
+  try {
+    const userMetadata = await metadata.findOne({ where: { client_id: proxy, user }});
+    if (userMetadata) {
+      /** Update */
+      await metadata.update({ user_metadata: newMetadata }, { where: { client_id: proxy, user }});
+    } else {
+      await metadata.create({
+        client_id: proxy,
+        user,
+        user_metadata: newMetadata
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  getUserMetadata,
+  updateUserMetadata,
+};
