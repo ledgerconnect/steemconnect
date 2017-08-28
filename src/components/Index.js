ce778817b675d06d57fb27c3b09bb9b9c065127b
adcpm <fabien@bonustrack.co>
2017-08-28 23:24:54 +0700

    Translate landing page

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { injectIntl, FormattedMessage } from 'react-intl';
 import { Form, Input, Button } from 'antd';
 import './Index.less';
 
@@ -17,7 +18,7 @@ class Index extends React.Component {
     this.props.form.validateFieldsAndScroll(() => {
       this.setState({ submitting: false });
     });
-  }
+  };
 
   render() {
     const { getFieldDecorator } = this.props.form;
@@ -33,27 +34,30 @@ class Index extends React.Component {
               </div>
               <div className="menu-item">
                 <a href="https://steemit.com/@steemconnect" target="_blank" rel="noreferrer noopener" className="follow-us">
-                  <span>Follow us on</span><object fill="#ffffff" data="img/steem.svg" type="image/svg+xml" />
+                  <span><FormattedMessage id="lp_follow_us" /></span>
+                  <object fill="#ffffff" data="img/steem.svg" type="image/svg+xml" />
                 </a>
               </div>
             </div>
             <div className="hero">
-              <h1 className="title">Plateform to Manage Your Steems</h1>
-              <h2 className="sub-title">Manage your Steem account and apps through a secure and independant plateform</h2>
+              <h1 className="title"><FormattedMessage id="lp_hero_title" /></h1>
+              <h2 className="sub-title"><FormattedMessage id="lp_hero_description" /></h2>
               <div className="newsletter">
                 <Form onSubmit={this.handleSubmit} layout="inline">
                   <Form.Item hasFeedback>
                     {getFieldDecorator('email', {
                       rules: [
-                        { type: 'email', message: 'Please input a valid email address' },
-                        { required: true, message: 'Please input your email address' },
+                        { type: 'email', message: this.props.intl.formatMessage({ id: 'error_invalid_email' }) },
+                        { required: true, message: this.props.intl.formatMessage({ id: 'error_empty_email' }) },
                       ],
                     })(
-                      <Input placeholder="Email address" />
+                      <Input placeholder={this.props.intl.formatMessage({ id: 'email_address' })} />
                     )}
                   </Form.Item>
                   <Form.Item>
-                    <Button type="primary" htmlType="submit" loading={this.state.submitting} className="lp-link">Sign up</Button>
+                    <Button type="primary" htmlType="submit" loading={this.state.submitting} className="lp-link">
+                      <FormattedMessage id="signup" />
+                    </Button>
                   </Form.Item>
                 </Form>
               </div>
@@ -62,33 +66,40 @@ class Index extends React.Component {
         </div>
 
         <div className="lp-container how-it-works">
-          <span className="small-title">How it works</span>
-          <h3>
-            The simple identity layer built on top of the Steem blockchain
-          </h3>
+          <span className="small-title">
+            <FormattedMessage id="lp_section_1_tag" />
+          </span>
+          <h3><FormattedMessage id="lp_section_1_title" /></h3>
         </div>
 
         <div className="steem-features-container">
           <div className="lp-container steem-features">
             <div className="steem-feature">
               <object data="img/apps.svg" type="image/svg+xml" />
-              <strong className="feature-title">Steem Apps</strong>
+              <strong className="feature-title">
+                <FormattedMessage id="lp_feature_1_title" />
+              </strong>
               <p className="feature-desc">
-                Select among various websites and apps using SteemConnect where you can easily log in with confidence
+                <FormattedMessage id="lp_feature_1_description" />
               </p>
             </div>
             <div className="steem-feature">
               <object data="img/account.svg" type="image/svg+xml" />
-              <strong className="feature-title">Manage Account</strong>
+              <strong className="feature-title">
+                <FormattedMessage id="lp_feature_2_title" />
+              </strong>
               <p className="feature-desc">
-                Access to authorized websites and apps with your Steem account credentials
-                using mechanisms for robust signing and encryption without giving to secure your password/key
+                <FormattedMessage id="lp_feature_2_description" />
               </p>
             </div>
             <div className="steem-feature">
               <object data="img/wallet.svg" type="image/svg+xml" />
-              <strong className="feature-title">Steem Wallet</strong>
-              <p className="feature-desc">Manage your wallet, check your activity on Steem and edit your Public Steem Profile</p>
+              <strong className="feature-title">
+                <FormattedMessage id="lp_feature_3_title" />
+              </strong>
+              <p className="feature-desc">
+                <FormattedMessage id="lp_feature_3_description" />
+              </p>
             </div>
           </div>
         </div>
@@ -99,42 +110,39 @@ class Index extends React.Component {
               <object data="img/lock.svg" type="image/svg+xml" />
             </div>
             <div>
-              <span className="small-title">Safe & Secure</span>
-              <h3>Your information have never been more secured</h3>
-              <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit
-              </p>
+              <span className="small-title">
+                <FormattedMessage id="lp_section_2_tag" />
+              </span>
+              <h3><FormattedMessage id="lp_section_2_title" /></h3>
+              <p><FormattedMessage id="lp_section_2_description" /></p>
             </div>
           </div>
         </div>
 
         <div className="lp-container learn-more">
-          <span className="small-title">Learn more</span>
-          <h3>Become part of the community</h3>
+          <span className="small-title">
+            <FormattedMessage id="lp_section_3_tag" />
+          </span>
+          <h3><FormattedMessage id="lp_section_3_title" /></h3>
         </div>
 
         <div className="lp-container project">
           <div className="project-item">
             <object data="img/opensource.svg" type="image/svg+xml" />
-            <div className="project-info">
-              <h4 className="project-title">It's an Open Source Project</h4>
-              <p>
-                Check the github repository<br />
-                and start digging
-              </p>
-              <a href="https://github.com/steemit/sc2" target="_blank" rel="noreferrer noopener" className="lp-link">Learn More</a>
+            <div>
+              <h4 className="project-title"><FormattedMessage id="lp_opensource_title" /></h4>
+              <p><FormattedMessage id="lp_opensource_description" /></p>
+              <a href="https://github.com/steemit/sc2" target="_blank" rel="noreferrer noopener" className="lp-link">
+                <FormattedMessage id="lp_opensource_button" />
+              </a>
             </div>
           </div>
           <div className="project-item">
             <object data="img/code.svg" type="image/svg+xml" />
-            <div className="project-info">
-              <h4 className="project-title">We Invite Developers</h4>
-              <p>
-                Start developing your app and rely on SteemConnect
-              </p>
-              <a href="/" className="lp-link">Register Now</a>
+            <div>
+              <h4 className="project-title"><FormattedMessage id="lp_developers_title" /></h4>
+              <p><FormattedMessage id="lp_developers_description" /></p>
+              <a href="/" className="lp-link"><FormattedMessage id="lp_developers_button" /></a>
             </div>
           </div>
         </div>
@@ -142,24 +150,24 @@ class Index extends React.Component {
         <div className="get-started-container">
           <div className="lp-container get-started">
             <div>
-              <h2>Register to Get Started</h2>
-              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusant doloremque laudantium, totam rem aperiam</p>
+              <h2><FormattedMessage id="lp_subscribe_title" /></h2>
+              <p><FormattedMessage id="lp_subscribe_description" /></p>
             </div>
             <div>
-              <a href="/" className="lp-link">Get Started Now</a>
+              <a href="/" className="lp-link">
+                <FormattedMessage id="lp_subscribe_button" />
+              </a>
             </div>
           </div>
         </div>
 
         <div className="lp-container footer-menu">
           <ul>
-            <li><Link to="/">About Us</Link></li>
+            <li><Link to="/"><FormattedMessage id="about_us" /></Link></li>
             <li className="separator">|</li>
-            <li><Link to="/">Careers</Link></li>
+            <li><Link to="/"><FormattedMessage id="terms" /></Link></li>
             <li className="separator">|</li>
-            <li><Link to="/">Terms of Services</Link></li>
-            <li className="separator">|</li>
-            <li><Link to="/">Privacy Policy</Link></li>
+            <li><Link to="/"><FormattedMessage id="privacy" /></Link></li>
           </ul>
         </div>
       </div>
@@ -167,4 +175,6 @@ class Index extends React.Component {
   }
 }
 
-export default Form.create()(Index);
+export default Form.create()(
+  injectIntl(Index)
+);
