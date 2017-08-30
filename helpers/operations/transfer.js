76fe26e9c6e2ba71fcf1f41503f4b95cdeb54922
Grégory LATINIER <cehraphaim@live.fr>
2017-08-30 16:09:46 +0200

    Add validation to transfer method

--- helpers/operations/transfer.js
+++ helpers/operations/transfer.js
@@ -1,14 +1,33 @@
 const _ = require('lodash');
+const { isAsset, isEmpty, userExists, normalizeUsername } = require('../validation-utils');
 
 const optionalFields = ['memo'];
 
 const parse = (query) => {
   const _query = _.cloneDeep(query);
+  const [amount, symbol] = _query.amount.split(' ');
+
+  _query.to = normalizeUsername(_query.to);
+  _query.amount = _.join([parseFloat(amount).toFixed(3), symbol], ' ');
   _query.memo = _query.memo || '';
+
   return _query;
+};
+
+const validate = async (query, errors) => {
+  if (!isEmpty(query.to) && !await userExists(query.to)) {
+    errors.push(`the user ${query.following} doesn't exist`);
+  }
+
+  if (!isEmpty(query.amount) && !['STEEM', 'SBD'].includes(query.amount.split(' ')[1])) {
+    errors.push('please select a valid symbol: STEEM or SBD');
+  } else if (!isEmpty(query.amount) && !isAsset(query.amount)) {
+    errors.push('please type a valid amount, 12.123 STEEM or 12.123 SBD for example');
+  }
 };
 
 module.exports = {
   optionalFields,
-  parse
+  parse,
+  validate
 };
