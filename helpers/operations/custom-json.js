7f4172ab5d4c8169422ab9a06a551d1cdc7b1d9f
Grégory LATINIER <cehraphaim@live.fr>
2017-09-05 11:14:16 +0200

    required_auths and required_posting_auths are JSON fields that needs to be parsed. adding JSON validation of those fields add user existence validation for required_posting_auths

--- helpers/operations/custom-json.js
+++ helpers/operations/custom-json.js
@@ -1,8 +1,43 @@
+const _ = require('lodash');
+const { userExists, isEmpty } = require('../validation-utils');
+
 const optionalFields = ['required_auths', 'required_posting_auths'];
 
-const parse = query => query;
+const parse = (query) => {
+  const cQuery = _.cloneDeep(query);
+  if (cQuery.required_auths) {
+    cQuery.required_auths = JSON.parse(cQuery.required_auths);
+  }
+  if (cQuery.required_posting_auths) {
+    cQuery.required_posting_auths = JSON.parse(cQuery.required_posting_auths);
+  }
+  return cQuery;
+};
+
+const validate = async (query, errors) => {
+  if (!isEmpty(query.required_auths)) {
+    try {
+      JSON.parse(query.required_auths);
+    } catch (err) {
+      errors.push('required_auths is not a JSON valid field');
+    }
+  }
+  if (!isEmpty(query.required_posting_auths)) {
+    try {
+      const auths = JSON.parse(query.required_posting_auths);
+      for (let i = 0; i < auths.length; i += 1) {
+        if (!await userExists(auths[i])) {
+          errors.push(`the user ${auths[i]} doesn't exist`);
+        }
+      }
+    } catch (err) {
+      errors.push('required_posting_auths is not a JSON valid field');
+    }
+  }
+};
 
 module.exports = {
   optionalFields,
   parse,
+  validate,
 };
