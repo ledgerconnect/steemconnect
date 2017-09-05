349898c7161b907024fc91b4df370013a0859388
Grégory LATINIER <cehraphaim@live.fr>
2017-09-05 16:06:32 +0200

    Error page

--- src/components/Sign/ValidationErrors.js
+++ src/components/Sign/ValidationErrors.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react';
-import Icon from '../../widgets/Icon';
 
 const SignValidationErrors = ({ errors }) =>
   <div>
-    <h2><Icon name="close" className="text-danger" lg /> Error</h2>
-    <h5 className="mb-4">Validation errors in your request!</h5>
+    <div className="Sign__error-title-bg">
+      <object data="/img/signin/fail.svg" type="image/svg+xml" id="error-icon" />
+    </div>
+    <h2>Error</h2>
+    <h5>Validation errors in your request!</h5>
     <table className="table text-center">
       <tbody>
         {errors.map((error, idx) =>
