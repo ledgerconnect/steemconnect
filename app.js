9e38886c3eceaf14fa38b72503f3ed9f523e3d6f
adcpm <fabien@bonustrack.co>
2017-10-12 02:59:19 +0700

    Remove unused dep

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const http = require('http');
