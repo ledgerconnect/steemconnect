150e8af7249a237a680258d718fa6633c8d720fb
Grégory <gregory.latinier@gmail.com>
2017-11-09 16:33:50 +0100

    Feature: display multiple operations

--- src/components/Sign.js
+++ src/components/Sign.js
@@ -4,6 +4,7 @@ import steem from 'steem';
 import changeCase from 'change-case';
 import { Link } from 'react-router';
 import { Button } from 'antd';
+import union from 'lodash/union';
 import SignForm from './Form/Sign';
 import SignSuccess from './Sign/Success';
 import SignError from './Sign/Error';
@@ -28,8 +29,10 @@ export default class Sign extends Component {
 
     this.state = {
       type: this.props.params.type,
+      base64: this.props.params.base64,
       query: this.props.location.query,
       normalizedQuery: null,
+      normalizedQueries: null,
       step: 'loading',
       success: false,
       error: false,
@@ -38,17 +41,75 @@ export default class Sign extends Component {
 
   async componentWillMount() {
     document.body.style.backgroundColor = '#f0f2f4';
-    const { type, query } = this.state;
-    if (getOperation(type) === '') {
-      this.props.router.push('/404');
-    }
-    const validationErrors = await validate(type, query);
-    if (validationErrors.length > 0) {
-      this.setState({ validationErrors, step: 'validationErrors' });
+    const { type, query, base64 } = this.state;
+    if (type === 'tx') {
+      if (!base64) {
+        this.setState({ validationErrors: [{ error: 'error_tx_base64_required' }], step: 'validationErrors' });
+      } else {
+        let operationsDecoded;
+        let operationsParsed;
+        try {
+          operationsDecoded = atob(base64);
+        } catch (err) {
+          this.setState({ validationErrors: [{ error: 'error_base64_encode' }], step: 'validationErrors' });
+          return;
+        }
+        try {
+          operationsParsed = JSON.parse(operationsDecoded);
+        } catch (err) {
+          this.setState({ validationErrors: [{ error: 'error_base64_json' }], step: 'validationErrors' });
+          return;
+        }
+        if (Array.isArray(operationsParsed)) {
+          let validationErrors = [];
+          for (let i = 0; i < operationsParsed.length; i += 1) {
+            validationErrors = validationErrors.concat(
+              await validate(
+                operationsParsed[i].operation,
+                operationsParsed[i].params || {}
+                )
+            );
+          }
+          if (validationErrors.length > 0) {
+            this.setState({ validationErrors, step: 'validationErrors' });
+          } else {
+            const normalizedQueries = [];
+            for (let i = 0; i < operationsParsed.length; i += 1) {
+              normalizedQueries.push({
+                operation: operationsParsed[i].operation,
+                params: operationsParsed[i].params,
+                normalizedQuery: await normalize(
+                  operationsParsed[i].operation,
+                  operationsParsed[i].params
+                ),
+              });
+            }
+            this.setState({ step: 'form', normalizedQueries });
+          }
+        } else {
+          this.setState({ validationErrors: [{ error: 'error_base64_encode' }], step: 'validationErrors' });
+        }
+      }
     } else {
-      const normalizedQuery = await normalize(type, query);
-      this.setState({ step: 'form', normalizedQuery });
+      if (getOperation(type) === '') {
+        this.props.router.push('/404');
+      }
+      const validationErrors = await validate(type, query);
+      if (validationErrors.length > 0) {
+        this.setState({ validationErrors, step: 'validationErrors' });
+      } else {
+        const normalizedQuery = await normalize(type, query);
+        this.setState({ step: 'form', normalizedQuery });
+      }
     }
+  }
+
+  getPlaceholder = (type) => {
+    let Placeholder = SignPlaceholderDefault;
+    Placeholder = (type === 'comment') ? SignPlaceholderComment : Placeholder;
+    Placeholder = (changeCase.snakeCase(type) === 'profile_update') ? SignPlaceholderNonFiltered : Placeholder;
+    Placeholder = (['transfer', 'delegate_vesting_shares', 'undelegate_vesting_shares'].includes(changeCase.snakeCase(type))) ? SignPlaceholderTransferDelegate : Placeholder;
+    return Placeholder;
   }
 
   resetForm = () => {
@@ -77,12 +138,19 @@ export default class Sign extends Component {
   };
 
   render() {
-    const { step, success, error, validationErrors, normalizedQuery, type } = this.state;
-    const op = getOperation(type);
-    let Placeholder = SignPlaceholderDefault;
-    Placeholder = (type === 'comment') ? SignPlaceholderComment : Placeholder;
-    Placeholder = (changeCase.snakeCase(type) === 'profile_update') ? SignPlaceholderNonFiltered : Placeholder;
-    Placeholder = (['transfer', 'delegate_vesting_shares'].includes(changeCase.snakeCase(type))) ? SignPlaceholderTransferDelegate : Placeholder;
+    const {
+      step, success, error, validationErrors, normalizedQuery, normalizedQueries, type,
+    } = this.state;
+    let op;
+    let roles = [];
+    let Placeholder;
+    if (normalizedQuery) {
+      op = getOperation(type);
+      roles = op.roles;
+      Placeholder = this.getPlaceholder(type);
+    } else if (normalizedQueries) {
+      normalizedQueries.map(query => (roles = union(roles, getOperation(query.operation).roles)));
+    }
     return (
       <div className="Sign">
         {step === 'loading' && <Loading />}
@@ -95,7 +163,7 @@ export default class Sign extends Component {
             <div className="Sign__signin-warning"><FormattedMessage id="steemconnect_website_confirm" /></div>}
             <div className="Sign__wrapper">
               {step === 'validationErrors' && <SignValidationErrors errors={validationErrors} />}
-              {step === 'form' &&
+              {step === 'form' && normalizedQuery &&
               <div className="Placeholder">
                 <h5><FormattedMessage id="confirmation_operation" /></h5>
                 <div className="Placeholder__operation-container">
@@ -107,7 +175,24 @@ export default class Sign extends Component {
                 </Button>
               </div>
               }
-              {step === 'signin' && <SignForm roles={op.roles} sign={this.sign} title={<FormattedMessage id="confirm_operation_log_in" />} />}
+              {step === 'form' && normalizedQueries &&
+              <div className="Placeholder">
+                <h5><FormattedMessage id="confirmation_operations" /></h5>
+                {normalizedQueries.map((query, idx) => {
+                  const opMulti = getOperation(query.operation);
+                  Placeholder = this.getPlaceholder(query.operation);
+                  return (
+                    <div className="Placeholder__operation-container" key={`operation_${query.operation}_${idx}`}>
+                      <h5 className="Placeholder__operation-title">{ changeCase.titleCase(query.operation) }</h5>
+                      <Placeholder query={query.normalizedQuery} params={opMulti.params} />
+                    </div>
+                  );
+                })}
+                <Button onClick={() => this.setState({ step: 'signin' })} type="primary" htmlType="button" className="SignForm__button">
+                  <FormattedMessage id="continue" />
+                </Button>
+              </div>}
+              {step === 'signin' && <SignForm roles={roles} sign={this.sign} title={<FormattedMessage id="confirm_operation_log_in" />} />}
               {step === 'signin' && <Link className="cancel-link" onClick={() => this.setState({ step: 'form' })}><FormattedMessage id="cancel" /></Link>}
               {step === 'result' && success && <SignSuccess result={success} cb={normalizedQuery.cb || normalizedQuery.redirect_uri} />}
               {step === 'result' && error && <SignError error={error} resetForm={this.resetForm} />}
