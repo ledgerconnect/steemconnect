8e1970863e833d828b0d4049f5422614e7fe0f20
Grégory <gregory.latinier@gmail.com>
2017-11-28 13:42:43 +0100

    Fix: add es6 polyfill

--- webpack/makeConfig.js
+++ webpack/makeConfig.js
@@ -91,8 +91,8 @@ function makeConfig(options) {
     devtool: isDevelopment ? 'cheap-eval-source-map' : 'source-map',
     entry: (isDevelopment ? [
       'webpack-hot-middleware/client?reload=true',
-      path.join(options.baseDir, 'node_modules/es6-object-assign/dist/object-assign-auto.js'),
-    ] : [path.join(options.baseDir, 'node_modules/es6-object-assign/dist/object-assign-auto.js')]).concat([
+      path.join(options.baseDir, 'node_modules/es6-shim/es6-shim.js'),
+    ] : [path.join(options.baseDir, 'node_modules/es6-shim/es6-shim.js')]).concat([
       path.join(options.baseDir, 'src/index.js'),
     ]),
     output: {
