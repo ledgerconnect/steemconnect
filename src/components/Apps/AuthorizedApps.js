2fbd2e2b96a28411a882671d2325e66b36a5e6a1
Grégory <gregory.latinier@gmail.com>
2017-11-30 15:38:12 +0100

    Restrict the display of authorized apps with the ones that steemconnect has in its database.

--- src/components/Apps/AuthorizedApps.js
+++ src/components/Apps/AuthorizedApps.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router';
 import numeral from 'numeral';
+import fetch from 'isomorphic-fetch';
 import Loading from '../../widgets/Loading';
 
 export default class AuthorizedApps extends Component {
@@ -15,11 +16,23 @@ export default class AuthorizedApps extends Component {
       error: false,
       isLoading: false,
       isLoaded: false,
+      apps: [],
     };
   }
 
+  async componentWillMount() {
+    const result = await fetch('/api/apps/authorized', {
+      headers: new Headers({
+        Authorization: this.props.auth.token,
+      }),
+    })
+    .then(res => res.json());
+
+    this.setState({ apps: result.apps });
+  }
+
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, apps } = this.state;
     const { user } = this.props.auth;
     return (
       <div className="container py-5">
@@ -29,14 +42,14 @@ export default class AuthorizedApps extends Component {
           <div>
             <p><FormattedMessage id="authorized_apps_list" /></p>
             <ul className="list-group text-xs-left mb-3">
-              {user.posting.account_auths.map((auth, idx) =>
+              {apps.map((app, idx) =>
                 <li key={idx} className="list-group-item">
-                  <b><Link to={`/apps/@${auth[0]}`}>{auth[0]}</Link></b>
+                  <b><Link to={`/apps/@${app[0]}`}>{app[0]}</Link></b>
                   <span className="ml-1">
-                    {numeral((100 / user.posting.weight_threshold) * (auth[1] / 100)).format('0%')}
+                    {numeral((100 / user.posting.weight_threshold) * (app[1] / 100)).format('0%')}
                   </span>
                   <Link
-                    to={`/revoke/@${auth[0]}`}
+                    to={`/revoke/@${app.client_id}`}
                     className="float-right btn btn-secondary btn-sm ml-1"
                   >
                     <FormattedMessage id="revoke" />
