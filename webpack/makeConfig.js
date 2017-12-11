1c019c092391f26f390612c683f09f0ede07de00
Grégory LATINIER <gregory.latinier@gmail.com>
2017-12-11 09:09:03 +0100

    Object assign issue (#154)
    * Merge dev
    
    * Fix issue (#146)
    
    * Feature:add french translation so that it makes more sense for the number format pull request
    
    * Add translation for the validation part
    
    * Add missing translation
    Add the same translation feature than in the faucet
    
    * Merge dev
    
    * Update to webpack 3 and small optimisation. 50ko less on the non gzip app.min.js
    
    * Fix: update yarn lock
    
    * Fix: typo error in translation key
    
    * Feature: Design and text UI for authorizing an app
    Complete French translation will be done in another PR
    
    * En language fix
    
    * Fix: Correct button style
    
    * Require only the needed methods of lodash
    
    * Fix: Same text for the offline access. Add the expiration at the bottom as a NB
    
    * Fix: Redirect to login page when the token is invalid
    => invalid token prevents the call to the /me endpoint and therefore populating the user object.
    => to keep the existing control, the test on the name attribute is still here
    
    * Fix: Redirect to login page when the token is invalid
    => invalid token prevents the call to the /me endpoint and therefore populating the user object.
    => to keep the existing control, the test on the name attribute is still here
    
    * Fix: Remove expiration sentence. Change offline to offline access
    
    * Fix: Change only the displayed name of the scope
    
    * Remove the insert of the operation
    operations model is therefore not needed
    
    * Feature: display the transaction ID and a link toward steemd
    Style: decrease the size the success and error icon
    
    * Feature: add sentry
    
    * Fix: nsp security issue
    
    * Fix: add input properties suggested by jm90m
    
    * Fix: style hide the horizontal scroll because of hero
    
    * Add missing description on hot signing link generator (#140)
    
    * Add hot signing operations description
    
    * Change translation key
    
    * Use json file for hot signing operations list and set keep the original order
    
    * Fix description transfer from savings
    
    * Token management improvement (#137)
    
    * Fix: handle token provided in the header like 'Bearer $token'
    
    * Fix: test only the header for Bearer, Basic string
    
    * Fix: simplify header token code
    
    * Fix: regex, ensure that the string replaced is at the beginning of the chain
    
    * Fix: Add space to regex
    
    * Fix: adding Async to the getConfig otherwise it returns a promise and not the data (#145)
    
    * Remove a comma to fix a syntax error (#147)
    
    'npm start' did not work due to a syntax error.
    
    * Fix: Object.assign undefined
    Modify the webpack bundle to ensure that the polyfill is at the beginning of the bundle
    
    * Fix: add es6 polyfill
    
    * Fix: add intl polyfill
    
    * Remove the use of polyfill and add Intl to build

--- webpack/makeConfig.js
+++ webpack/makeConfig.js
@@ -91,7 +91,12 @@ function makeConfig(options) {
     devtool: isDevelopment ? 'cheap-eval-source-map' : 'source-map',
     entry: (isDevelopment ? [
       'webpack-hot-middleware/client?reload=true',
-    ] : []).concat([
+      path.join(options.baseDir, 'node_modules/es6-shim/es6-shim.js'),
+      path.join(options.baseDir, 'node_modules/intl/dist/Intl.js'),
+    ] : [
+      path.join(options.baseDir, 'node_modules/es6-shim/es6-shim.js'),
+      path.join(options.baseDir, 'node_modules/intl/dist/Intl.js'),
+    ]).concat([
       path.join(options.baseDir, 'src/index.js'),
     ]),
     output: {
