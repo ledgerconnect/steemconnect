a6c6111841773828530d74c84cf41337a972aae6
Fabien <bonustrack@users.noreply.github.com>
2018-01-26 22:32:16 +0700

    Change user_metadata field to TEXT type instead of JSONB (#181)
    * Log error related to usermetadata
    
    * Add db pool settings
    
    * Select user_metadata
    
    * Change JSONB to TEXT
    
    * Change JSONB to TEXT in other tables
    
    * redirect_url field require JSONB
    
    * Revert
    
    * Apply stringify and parse on user_metadata
    
    * Revert pool
    
    * Add pool db setting

--- helpers/metadata.js
+++ helpers/metadata.js
@@ -1,13 +1,18 @@
 const { metadata } = require('../db/models');
+const debug = require('debug')('sc2:server');
 
 /** Get user_metadata */
 const getUserMetadata = async (proxy, user) => {
   try {
-    const userMetadata = await metadata.findOne({ where: { client_id: proxy, user } });
+    const userMetadata = await metadata.findOne({
+      attributes: ['user_metadata'],
+      where: { client_id: proxy, user },
+    });
     if (userMetadata) {
-      return userMetadata.user_metadata;
+      return JSON.parse(userMetadata.user_metadata);
     }
   } catch (error) {
+    debug('getUserMetadata failed', error);
     throw new Error(error);
   }
   return {};
@@ -16,18 +21,25 @@ const getUserMetadata = async (proxy, user) => {
 /** Update user_metadata */
 const updateUserMetadata = async (proxy, user, newMetadata) => {
   try {
-    const userMetadata = await metadata.findOne({ where: { client_id: proxy, user } });
+    const userMetadata = await metadata.findOne({
+      attributes: ['user_metadata'],
+      where: { client_id: proxy, user },
+    });
     if (userMetadata) {
       /** Update */
-      await metadata.update({ user_metadata: newMetadata }, { where: { client_id: proxy, user } });
+      await metadata.update(
+        { user_metadata: JSON.stringify(newMetadata) },
+        { where: { client_id: proxy, user } },
+      );
     } else {
       await metadata.create({
         client_id: proxy,
         user,
-        user_metadata: newMetadata,
+        user_metadata: JSON.stringify(newMetadata),
       });
     }
   } catch (error) {
+    debug('updateUserMetadata failed', error);
     throw new Error(error);
   }
 };
