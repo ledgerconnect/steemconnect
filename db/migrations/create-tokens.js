649a977f82a7ca9549c6e1ba0bed8b59bfe42847
Grégory LATINIER <gregory.latinier@gmail.com>
2018-04-17 13:12:35 +0200

    Add triggers to migrate scripts (#231)
    * Add triggers to migrate scripts
    
    * Do not allow more than 20 tokens at the same time

--- db/migrations/create-tokens.js
+++ db/migrations/create-tokens.js
@@ -27,6 +27,29 @@ module.exports = {
       type: Sequelize.DATE,
     },
   })
-    .then(() => queryInterface.addIndex('tokens', { fields: ['token'], unique: true })),
-  down: queryInterface => queryInterface.dropTable('tokens'),
+    .then(() => {
+      queryInterface.addIndex('tokens', { fields: ['token'], unique: true });
+      queryInterface.sequelize.query('CREATE FUNCTION public.delete_expired_tokens_func()\n' +
+      'RETURNS trigger\n' +
+      'LANGUAGE \'plpgsql\'\n' +
+      'COST 100\n' +
+      'VOLATILE NOT LEAKPROOF\n' +
+      'AS $BODY$\n' +
+      'BEGIN\n' +
+      'DELETE FROM tokens WHERE created_at < NOW() - INTERVAL \'8 days\';\n' +
+      'DELETE FROM tokens WHERE id IN(SELECT id FROM tokens WHERE client_id = new."client_id" AND "user" = new."user" ORDER BY created_at DESC OFFSET 20);\n' +
+      'RETURN NULL;\n' +
+      'END;\n' +
+      '$BODY$;');
+      queryInterface.sequelize.query('CREATE TRIGGER delete_expired_tokens_trigger\n' +
+        'AFTER INSERT\n' +
+        'ON public.tokens\n' +
+        'FOR EACH ROW\n' +
+        'EXECUTE PROCEDURE public.delete_expired_tokens_func();');
+    }),
+  down: (queryInterface) => {
+    queryInterface.dropTable('tokens');
+    queryInterface.sequelize.query('DROP TRIGGER delete_expired_tokens_trigger ON public.tokens;');
+    queryInterface.sequelize.query('DROP FUNCTION public.delete_expired_tokens_func();');
+  },
 };
