3a6adf248c3f06c427c15c23b6f6ffc86c87f561
Grégory <gregory.latinier@gmail.com>
2018-05-18 10:46:43 +0200

    Add js version to force refresh on browser when releasing the app version field in package json must be updated before doing a release in a dedicated PR along with creating a tag

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.get('/*', (req, res) => {
-  res.render('index', { title: 'SteemConnect' });
+  res.render('index', {
+    title: 'SteemConnect',
+    jsversion: process.env.NODE_ENV === 'production' ? process.env.npm_package_version : new Date().getTime(),
+  });
 });
 
 module.exports = router;
