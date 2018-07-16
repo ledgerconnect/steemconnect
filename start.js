3b3352f4ae9e7045962bddeeb6dd5df27b636651
adcpm <fabien@bonustrack.co>
2018-07-17 00:33:58 +0200

    Init server

---
+++ start.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+
+const app = express();
+app.use(serveStatic(`${__dirname}/dist`));
+
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
