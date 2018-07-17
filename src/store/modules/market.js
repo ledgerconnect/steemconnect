b754eb4f78eac4208297787662f9cff286c5078f
adcpm <fabien@bonustrack.co>
2018-07-17 13:54:01 +0200

    Add order book

--- src/store/modules/market.js
+++ src/store/modules/market.js
@@ -4,32 +4,46 @@ import Client from 'lightrpc';
 const client = new Client('https://api.steemit.com');
 
 const state = {
-  recentTrades: [],
   ticker: {},
+  orderBook: {
+    bids: [],
+    asks: [],
+  },
+  recentTrades: [],
 };
 
 const mutations = {
-  saveRecentTrades(_state, result) {
-    if (result) {
-      Vue.set(state, 'recentTrades', result);
-    }
-  },
   saveTicker(_state, result) {
     if (result) {
       Vue.set(state, 'ticker', result);
     }
   },
+  saveOrderBook(_state, result) {
+    if (result) {
+      Vue.set(state, 'orderBook', result);
+    }
+  },
+  saveRecentTrades(_state, result) {
+    if (result) {
+      Vue.set(state, 'recentTrades', result);
+    }
+  },
 };
 
 const actions = {
-  getRecentTrades: ({ commit }) => {
-    client.call('get_recent_trades', [25], (err, result) => {
-      commit('saveRecentTrades', result);
-    });
-  },
   getTicker: ({ commit }) => {
     client.call('get_ticker', [], (err, result) => {
       commit('saveTicker', result);
+    });
+  },
+  getOrderBook: ({ commit }) => {
+    client.call('get_order_book', [500], (err, result) => {
+      commit('saveOrderBook', result);
+    });
+  },
+  getRecentTrades: ({ commit }) => {
+    client.call('get_recent_trades', [25], (err, result) => {
+      commit('saveRecentTrades', result);
     });
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,10 +10,39 @@
         <div>24h volume STEEM {{ticker.steem_volume}}</div>
         <div>24h volume SBD {{ticker.sbd_volume}}</div>
       </div>
+      <h2>Order book</h2>
+      <div>
+        <h3>Bids (buy orders)</h3>
+        <div
+          v-for="(order, key, i) in orderBook.bids.slice(0, 20)"
+          :key="i"
+        >
+          {{order.created | date}}
+          {{order.sbd / 1000}}
+          {{order.steem / 1000}}
+          {{order.order_price.base}}
+          {{order.order_price.quote}}
+          {{order.real_price}}
+        </div>
+      </div>
+      <div>
+        <h3>Asks (sell orders)</h3>
+        <div
+          v-for="(order, key, i) in orderBook.asks.slice(0, 20)"
+          :key="i"
+        >
+          {{order.created | date}}
+          {{order.sbd / 1000}}
+          {{order.steem / 1000}}
+          {{order.order_price.base}}
+          {{order.order_price.quote}}
+          {{order.real_price}}
+        </div>
+      </div>
       <h2>Trade history</h2>
       <div>
         <div
-          v-for="(trade, key, i) in recentTrades"
+          v-for="(trade, key, i) in recentTrades.slice(0, 20)"
           :key="i"
         >
           {{trade.date | date}}
@@ -31,20 +60,25 @@ import { mapActions } from 'vuex';
 
 export default {
   computed: {
-    recentTrades() {
-      return this.$store.state.market.recentTrades;
-    },
     ticker() {
       return this.$store.state.market.ticker;
     },
+    orderBook() {
+      return this.$store.state.market.orderBook;
+    },
+    recentTrades() {
+      return this.$store.state.market.recentTrades;
+    },
   },
   methods: mapActions([
-    'getRecentTrades',
     'getTicker',
+    'getOrderBook',
+    'getRecentTrades',
   ]),
   created() {
-    this.getRecentTrades();
     this.getTicker();
+    this.getOrderBook();
+    this.getRecentTrades();
   },
 };
 </script>
