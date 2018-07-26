874d64bce98e5b31fb668a7dd492f1c3a27bb42e
adcpm <fabien@bonustrack.co>
2018-07-26 14:46:31 +0200

    Improve sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,16 @@
 <template>
   <div id="app" class="d-flex flex-row">
-    <div id="nav" class="p-3 border-right">
-      <ul>
-        <li>
-          <router-link to="/">Home</router-link>
-        </li>
-        <li>
-          Markets
-          <ul>
-            <li>
-              <router-link to="/market/SBD">SBD</router-link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <router-link to="/about">About</router-link>
-        </li>
-      </ul>
-    </div>
-    <div id="content" class="width-full p-3">
+    <Sidebar/>
+    <div id="content" class="width-full p-4">
       <router-view/>
     </div>
   </div>
 </template>
 
 <style scoped lang="less">
-#nav {
-  background-color: white;
-  width: 200px;
-  ul {
-    list-style: none;
-    li {
-      clear: both;
-    }
-  }
+@import './vars';
+
+#content {
+  margin-left: @sidebar-width;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="nav" class="border-right">
+    <div class="p-4">
+      <span id="logo" class="octicon octicon-diff-modified"/>
+    </div>
+    <ul>
+      <li>
+        <router-link to="/" class="py-2 px-4 d-block">
+          Home
+        </router-link>
+      </li>
+      <li class="border-top py-3">
+        <div class="pb-1 px-4 text-uppercase text-small">Markets</div>
+        <ul>
+          <li>
+            <router-link to="/market/SBD" class="py-1 px-4 d-block">
+              SBD
+            </router-link>
+          </li>
+          <li class="py-1 px-4">...</li>
+        </ul>
+      </li>
+      <li class="py-2 px-4 border-top">
+        <router-link to="/about">About</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import '../vars';
+
+#logo {
+  font-size: 32px;
+}
+
+#nav {
+  font-size: 18px;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: @sidebar-width;
+  overflow: auto;
+  background-color: @sidebar-bg-color;
+  ul {
+    list-style: none;
+    li {
+      clear: both;
+    }
+  }
+}
+</style>
