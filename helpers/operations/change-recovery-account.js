446fe047dbba420439cdce3d1c2e321d9589c51a
adcpm <fabien@bonustrack.co>
2018-08-16 10:49:57 +0200

    Add safe parse helper

--- helpers/operations/change-recovery-account.js
+++ helpers/operations/change-recovery-account.js
@@ -1,11 +1,12 @@
 const cloneDeep = require('lodash/cloneDeep');
 const { isEmpty, userExists, normalizeUsername } = require('../validation-utils');
+const { jsonParse } = require('../utils');
 
 const parse = (query) => {
   const cQuery = cloneDeep(query);
   cQuery.account_to_recover = normalizeUsername(cQuery.account_to_recover);
   cQuery.new_recovery_account = normalizeUsername(cQuery.new_recovery_account);
-  cQuery.extensions = query.extensions ? JSON.parse(query.extensions) : [];
+  cQuery.extensions = query.extensions ? jsonParse(query.extensions) : [];
 
   return cQuery;
 };
