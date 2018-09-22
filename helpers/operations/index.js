c859e5ffbbd9e8d37a4d90cdea9893eed95c5d85
Fabien <bonustrack@users.noreply.github.com>
2018-09-22 16:00:30 +0700

    Fix operation "transfer_from_savings" (#331)

--- helpers/operations/index.js
+++ helpers/operations/index.js
@@ -17,6 +17,7 @@ const unfollow = require('./unfollow');
 const unmute = require('./unmute');
 const vote = require('./vote');
 const convert = require('./convert');
+const transferFromSavings = require('./transfer-from-savings');
 
 module.exports = {
   comment,
@@ -38,4 +39,5 @@ module.exports = {
   profile_update: profileUpdate,
   vote,
   convert,
+  transfer_from_savings: transferFromSavings,
 };
