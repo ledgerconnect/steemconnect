9733b2c65e917692d7e4bb970acb4b106cf0f491
adcpm <fabien@bonustrack.co>
2018-09-28 19:57:08 +0700

    Fix app account creation

--- src/components/Apps/CreateApp.js
+++ src/components/Apps/CreateApp.js
@@ -7,7 +7,6 @@ import { browserHistory } from 'react-router';
 import CreateAppForm from '../Form/CreateApp';
 import SignForm from '../Form/Sign';
 import config from '../../../config.json';
-import { getAccountCreationFee } from '../../utils/auth';
 import { getErrorMessage } from '../../../helpers/operation';
 import Loading from '../../widgets/Loading';
 import { sleep } from '../../../helpers/utils';
@@ -25,7 +24,7 @@ class CreateApp extends React.Component {
   };
 
   componentWillMount = async () => {
-    const accountCreationFee = await getAccountCreationFee();
+    const accountCreationFee = '3.000 STEEM';
     this.setState({ accountCreationFee });
   };
 
@@ -47,9 +46,7 @@ class CreateApp extends React.Component {
     this.hideModal();
     this.setState({ isLoading: true });
     const clientId = this.state.values.username;
-
-    /** Calculate required STEEM to create new account */
-    const accountCreationFee = await getAccountCreationFee();
+    const accountCreationFee = '3.000 STEEM';
 
     /** Generate account authorities */
     const publicKeys = config.offline_generated_public_keys;
@@ -57,54 +54,60 @@ class CreateApp extends React.Component {
     const active = { weight_threshold: 1, account_auths: [['steemconnect', 1]], key_auths: [[publicKeys.active, 1]] };
     const posting = { weight_threshold: 1, account_auths: [['steemconnect', 1]], key_auths: [[publicKeys.posting, 1]] };
 
-    /** Create proxy account */
-    await steem.broadcast.accountCreateWithDelegationAsync(
-      auth.wif,
-      accountCreationFee,
-      '0.000000 VESTS',
-      auth.username,
-      clientId,
-      owner,
-      active,
-      posting,
-      publicKeys.memo,
-      { owner: this.props.auth.user.name },
-      []
-    ).then(async () => {
-      /** Wait 5 seconds to insure the newly created account is indexed on the node */
-      await sleep(5000);
+    const operations = [[
+      'account_create', {
+        fee: accountCreationFee,
+        creator: auth.username,
+        new_account_name: clientId,
+        owner,
+        active,
+        posting,
+        memo_key: publicKeys.memo,
+        json_metadata: JSON.stringify({ owner: this.props.auth.user.name }),
+      },
+    ]];
 
-      /** Send request to server for create app */
-      fetch(`/api/apps/@${clientId}`, {
-        headers: new Headers({
-          Authorization: this.props.auth.token,
-        }),
-        method: 'POST',
-      })
-        .then(res => res.json())
-        .then((data) => {
-          if (!data.error) {
-            /** Redirect to edit app */
-            browserHistory.push(`/apps/@${clientId}/edit`);
-            notification.success({
-              message: intl.formatMessage({ id: 'success' }),
-              description: intl.formatMessage({ id: 'success_proxy_account' }, { clientId }),
+    /** Create proxy account */
+    steem.broadcast.send(
+      { operations, extensions: [] },
+      { active: auth.wif },
+      async (err) => {
+        if (!err) {
+          /** Wait 5 seconds to insure the newly created account is indexed on the node */
+          await sleep(5000);
+
+          /** Send request to server for create app */
+          fetch(`/api/apps/@${clientId}`, {
+            headers: new Headers({
+              Authorization: this.props.auth.token,
+            }),
+            method: 'POST',
+          })
+            .then(res => res.json())
+            .then((data) => {
+              if (!data.error) {
+                /** Redirect to edit app */
+                browserHistory.push(`/apps/@${clientId}/edit`);
+                notification.success({
+                  message: intl.formatMessage({ id: 'success' }),
+                  description: intl.formatMessage({ id: 'success_proxy_account' }, { clientId }),
+                });
+              } else {
+                this.setState({ isLoading: false });
+                notification.error({
+                  message: intl.formatMessage({ id: 'error' }),
+                  description: data.error || intl.formatMessage({ id: 'general_error' }),
+                });
+              }
             });
-          } else {
-            this.setState({ isLoading: false });
-            notification.error({
-              message: intl.formatMessage({ id: 'error' }),
-              description: data.error || intl.formatMessage({ id: 'general_error' }),
-            });
-          }
-        });
-    }).catch((err) => {
-      this.setState({ isLoading: false });
-      notification.error({
-        message: intl.formatMessage({ id: 'error' }),
-        description: getErrorMessage(err) || intl.formatMessage({ id: 'general_error' }),
+        } else {
+          this.setState({ isLoading: false });
+          notification.error({
+            message: intl.formatMessage({ id: 'error' }),
+            description: getErrorMessage(err) || intl.formatMessage({ id: 'general_error' }),
+          });
+        }
       });
-    });
   };
 
   render() {
