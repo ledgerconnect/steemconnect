04d6200056731c5c9cc6fada3fa96c8258e4c625
adcpm <fabien@bonustrack.co>
2018-10-02 12:41:28 +0700

    Remove TradingView

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,6 @@ module.exports = {
     'plugin:vue/essential',
     '@vue/airbnb'
   ],
-  globals: {
-    'TradingView': true,
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
