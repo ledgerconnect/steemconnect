6a6f0ed0b3c9f2079025c2f59d212c082fd26f4c
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-11-01 17:08:03 +0100

    make operation box full-width (#24)
    * make operation box full-width
    
    * increase negative margin

--- src/components/Operation.vue
+++ src/components/Operation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Box mb-4">
+  <div class="Box operation mb-4">
     <OperationHeader :operation="operation[0]"/>
     <div class="Box-row">
       <div v-for="(value, key) in operation[1]" :key="key">
