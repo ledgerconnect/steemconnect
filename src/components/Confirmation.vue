22b690db9694a9eb0926c7b1d0be45bf5b4c764f
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-12-06 12:19:51 +0100

    feat: add close button on confirmation page (#43)

--- src/components/Confirmation.vue
+++ src/components/Confirmation.vue
@@ -2,13 +2,33 @@
   <div class="flash flash-success mb-4">
     Your transaction is on the way! Here is the ID of the transaction:
     <a :href="`https://steemd.com/tx/${id}`" target="_blank">{{ id }}</a>
+    <button
+      v-if="isExtension"
+      type="submit"
+      class="btn btn-large btn-success mt-4"
+      @click="close"
+    >
+      Close
+    </button>
   </div>
 </template>
 
 <script>
+import { isChromeExtension } from '@/helpers/utils';
+
 export default {
   props: {
     id: String,
+  },
+  data() {
+    return {
+      isExtension: isChromeExtension(),
+    };
+  },
+  methods: {
+    close() {
+      window.close();
+    },
   },
 };
 </script>
