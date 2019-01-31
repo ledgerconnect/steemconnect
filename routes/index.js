078ed2bc322c2a9e53c9ed20362646adf7497460
adcpm <fabien@bonustrack.co>
2019-01-31 16:54:18 +0700

    Remove API routes

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router(); // eslint-disable-line new-cap
-
-router.get('/*', (req, res) => {
-  res.render('index', { title: 'SteemConnect' });
-});
-
-module.exports = router;
