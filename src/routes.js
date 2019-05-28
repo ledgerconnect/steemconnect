4dd38931e8587cc1ae5c6bf2ffe0950d4a0b92b9
adcpm <fabien@bonustrack.co>
2019-05-28 19:50:27 +0700

    Let sign component on v2

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import App from './components/Apps/App';
 // import Apps from './components/Apps/Apps';
 import AuthorizedApps from './components/Apps/AuthorizedApps';
 import Steemjs from './components/Docs/Steemjs';
-// import Sign from './components/Sign';
+import Sign from './components/Sign';
 // import Generate from './components/GenerateLink';
 // import RecoverAccount from './components/Accounts/RecoverAccount';
 // import RequestAccountRecovery from './components/Accounts/RequestAccountRecovery';
@@ -49,7 +49,7 @@ export default (
     <Route path="/login" component={Login} />
     <Route path="/oauth2/authorize" component={Authorize} />
     <Route path="/sign" component={redirectToNewVersion} />
-    <Route path="/sign/:type(/:base64)" component={redirectToNewVersion} />
+    <Route path="/sign/:type(/:base64)" component={Sign} />
     <Route path="/authorize/@:username" component={redirectToNewVersion} />
     <Route path="/authorize/@:username/:role" component={redirectToNewVersion} />
     <Route path="/revoke/@:username" component={redirectToNewVersion} />
