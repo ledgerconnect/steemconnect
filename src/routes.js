85973bd73f17b00d3420c62774e83c5d6c48dd4e
adcpm <fabien@bonustrack.co>
2019-07-03 00:41:44 +0700

    Redirect v2 subdomain too

--- src/routes.js
+++ src/routes.js
@@ -25,6 +25,7 @@ const redirectToNewVersion = () => {
   window.location = (window.location.href)
     .replace('http://localhost:3000', 'https://beta.steemconnect.com')
     .replace('https://app.steemconnect.com', 'https://beta.steemconnect.com')
+    .replace('https://v2.steemconnect.com', 'https://beta.steemconnect.com')
     .replace('https://steemconnect.com', 'https://beta.steemconnect.com');
 };
 
