a1b5545efaf64d492fcc9ad714f481662be8248b
adcpm <fabien@bonustrack.co>
2019-07-03 09:12:38 +0700

    Fix route name

--- src/router.js
+++ src/router.js
@@ -101,7 +101,7 @@ export default new Router({
     },
     {
       path: '/sign/profile-update',
-      name: 'sign',
+      name: 'profile-update',
       beforeEnter: () => {
         window.location.href = `https://v2.steemconnect.com${window.location.pathname}${
           window.location.search
