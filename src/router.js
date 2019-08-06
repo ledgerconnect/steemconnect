8521eaaf22b9153eeab5a0a5d0f752d30518c47c
adcpm <fabien@bonustrack.co>
2019-08-06 09:29:50 +0700

    Redirect v2 routes

--- src/router.js
+++ src/router.js
@@ -72,6 +72,14 @@ export default new Router({
       component: isWeb() ? Home : Dashboard,
     },
     {
+      path: '/v2',
+      alias: ['/sign', '/sign/profile-update', '/accounts/create'],
+      beforeEnter: () => {
+        const { pathname, search } = window.location;
+        window.location.href = `https://v2.steemconnect.com${pathname}${search}`;
+      },
+    },
+    {
       path: '/import',
       name: 'import',
       component: Import,
@@ -101,14 +109,6 @@ export default new Router({
       path: '/login-request/:clientId',
       name: 'login-request-app',
       component: LoginRequest,
-    },
-    {
-      path: '/sign/profile-update',
-      beforeEnter: () => {
-        window.location.href = `https://v2.steemconnect.com${window.location.pathname}${
-          window.location.search
-        }`;
-      },
     },
     {
       path: '/sign/*',
